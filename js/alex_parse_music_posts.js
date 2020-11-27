ec74f6538dae9791c58b77bc926dbd64ab4cfdcc
Alex Page <alexanderkpage@gmail.com>
2020-11-27 14:32:47 -0500

    adding comments, adding if logic in case there is no spotify link

--- js/alex_parse_music_posts.js
+++ js/alex_parse_music_posts.js
@@ -4,20 +4,35 @@
 function displayPosts(postPage) {
 	console.log(postPage);
 	//console.log(postData); 
+	
 	var postCounter = 1;
+	
 	postData.forEach (entry => {
-		if (entry.post_page === postPage) {
+		if (entry.post_page === postPage) { // this is how we filter per page!
 			console.log("Matched on entry");
 			console.log(entry);
-			var postId = 'post'+postCounter;
-			$("#postSection").append('<div class="post-container" id="'+postId+'"></div>');
+			
+			// we will use this to append the html to the right element, its how we 'group' the data
+			var postId = 'post'+postCounter; 
+			
+			$("#postSection").append('<div class="post-container" id="'+postId+'"></div>'); // notice the element ID attr is set to the postId counter variable! that is how we can dynamically find the right div to append the song post data to!
+			
 			$("#"+postId).append('<h3>Song: '+entry.song+'</h3>');
 			$("#"+postId).append('<h4>Artist: '+entry.artist+'</h4>');
-			$("#"+postId).append('<iframe src="'+entry.spotify_embed_link+'" width="300" height="80" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>');
+			
+			// what if we didn't supply a spotify embed link? no prob, only print this if we have one - you may want to employ this kind of simple 'if' logic for other fields
+			if (entry.spotify_embed_link != null)  {
+				$("#"+postId).append('<iframe src="'+entry.spotify_embed_link+'" width="300" height="80" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>');
+			}
+			
+			// this will iterate over the array of description lines, so doesn't care how many/few there are, it prints each element as its own p tag
 			entry.description_lines.forEach (descLine => {
 				$("#"+postId).append('<p>'+descLine+'</p>');
 			});
+			
 			$("#postSection").append('<br/><hr/><br/>');
+			
+			// increase the counter for the next iteration!
 			postCounter++;
 		}
 	});
@@ -46,6 +61,16 @@ var postData = [
 				"Woo hoo my second post now i need more text, I am jus going to repeat myself cuz I am bored. Woo hoo more text, I am jus going to repeat myself cuz I am bored. Woo hoo more text, I am jus going to repeat myself cuz I am bored. ",
 				"This is my second paragraph about this stupid ass emo song. Woo hoo more text, I am jus going to repeat myself cuz I am bored. Woo hoo more text, I am jus going to repeat myself cuz I am bored. End of second paragraph for second post",
 				"And here is a third line! I can print as many desc lines as you give me! <i>Here is some italic. </i><b>Here is some bold</b> - but really you shouldn't use i/b tags, you should use <a href='https://stackoverflow.com/questions/38205797/how-to-combine-bold-and-italic-in-css/38205861'>CSS classes</a>  instead!"
+			]
+		},
+		{ 
+			"song" : "My Unknown Song",
+			"artist" : "Ale Alejandro",
+			"post_page" : "2020-11-26",
+			"description_lines" : [
+				"Nothin much to say, this song isn't real.",
+				"Notice how I did not print a spotify embed link cuz this one don't got one in the json!",
+				"I know its big, but I didn't pull it out for air.."
 			]
 		},
 		{ 
