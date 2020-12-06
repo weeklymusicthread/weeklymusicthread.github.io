4ae3fd53de98d67681bf8f3a55109942c150ccae
Sam <sam.paderewski@gmail.com>
2020-12-05 20:26:26 -0500

    add arist hyperlink search

---
+++ js/parse_search.js
@@ -0,0 +1,41 @@
+function searchName() {
+	const queryString = window.location.search;
+	const urlParams = new URLSearchParams(queryString)
+	const searchedArtist = urlParams.get('artist')
+	console.log(searchedArtist)
+
+	$("#postHeading").append('<h1>WMT Entries for '+searchedArtist+'</h1>');
+
+	var postCounter = 1;
+	
+	// jquery function to get the json file and then put it into memory as "postData" var
+	$.getJSON('http://weeklymusicthread.com/music_entries.json', postData => {
+	
+		postData.forEach (entry => {
+			if (entry.artist === searchedArtist) { 
+				console.log("Matched on entry");
+				console.log(entry);
+			
+				var postId = 'post'+ postCounter; 
+				
+				$("#postSection").append('<div class="post-container" id="'+postId+'"></div>'); 
+				
+				$("#"+postId).append('<p><u> "'+entry.song+''+'" by '+entry.artist+' </u>' + '&nbsp; <span style = "color:rgb(180, 180, 180)">(posted on '+entry.post_page + ')</span></p>');
+				
+				if (entry.spotify_id != null)  {
+					console.log('contains spotify ID: '+entry.spotify_id);
+					$("#"+postId).append('<div style="float: left; clear: left; padding-right: 2em;"><iframe src="https://open.spotify.com/embed/track/'+entry.spotify_id+'" width="300" height="380" scrolling="no" frameborder="0" float="right" allowtransparency="true" allow="encrypted-media"></iframe></div>');
+						
+				}
+				
+				entry.description_lines.forEach (descLine => {
+					$("#"+postId).append('<p>'+descLine+'</p>');
+				});
+				
+				$("#postSection").append('<br/>');
+				
+				postCounter++;
+			}
+		});
+	});
+}
