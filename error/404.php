a2e3740cbfc0d8bd809cdac3980a76a09cbaa7f1
Alex Page <alexanderkpage@gmail.com>
2020-12-08 10:35:45 -0500

    fixing paths

--- error/404.php
+++ error/404.php
@@ -8,15 +8,15 @@
   <meta name="description" content="">
   <meta name="author" content="">
   
-  <link rel="icon" type="image/x-icon" href="img/favicon.ico" />
+  <link rel="icon" type="image/x-icon" href="/img/favicon.ico" />
 
   <title>Weekly Music Thread</title>
 
   <!-- Bootstrap core CSS -->
-  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 
   <!-- Custom fonts for this template -->
-  <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+  <link href="/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
   <link href='https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
   <link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
 
@@ -27,10 +27,10 @@
 
 <body>
 
-	<?php include('php/navbar.php'); ?>
+	<?php include('/php/navbar.php'); ?>
 
   <!-- Page Header -->
-  <header class="masthead" style="background-image: url('img/vaporheader.png')">
+  <header class="masthead" style="background-image: url('/img/vaporheader.png')">
     <div class="overlay"></div>
     <div class="container">
       <div class="row">
@@ -98,11 +98,11 @@
   </footer>
 
   <!-- Bootstrap core JavaScript -->
-  <script src="vendor/jquery/jquery.min.js"></script>
-  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="/vendor/jquery/jquery.min.js"></script>
+  <script src="/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
   <!-- Custom scripts for this template -->
-  <script src="js/clean-blog.min.js"></script>
+  <script src="/js/clean-blog.min.js"></script>
 
 </body>
 
