dda779d93323c215f8b65a30a28e31e794dd5686
Sam <sam.paderewski@gmail.com>
2020-12-08 23:29:41 -0500

    fix preview test

--- index_test.php
+++ index_test.php
@@ -69,7 +69,7 @@
   <!-- Custom scripts for this template -->
   <script src="js/clean-blog.min.js"></script>
 
-  <script src="js/parse_post_previews.js"></script>
+  <script src="js/parse_post_previews.js?newversion"></script>
 
   <script> displayPreviews();  </script>
 
