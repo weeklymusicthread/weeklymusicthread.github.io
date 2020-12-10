fb53bce7b70a6f2df075088ae353e7d621e8908a
Sam <sam.paderewski@gmail.com>
2020-12-10 14:22:15 -0500

    add subscribe nav and foot

--- php/footer.php
+++ php/footer.php
@@ -47,6 +47,25 @@
 						</a>
 					</li>
 				</ul>
+				<!-- Begin Mailchimp Signup Form -->
+				<link href="//cdn-images.mailchimp.com/embedcode/horizontal-slim-10_7.css" rel="stylesheet" type="text/css">
+				<style type="text/css">
+					#mc_embed_signup{background:#fff; clear:left; font:14px Helvetica,Arial,sans-serif; width:100%;}
+					/* Add your own Mailchimp form style overrides in your site stylesheet or in this style block.
+					We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
+				</style>
+				<div id="mc_embed_signup">
+				<form action="https://weeklymusicthread.us7.list-manage.com/subscribe/post?u=06562c29028f6e0751eee5238&amp;id=d5c5306a2e" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+					<div id="mc_embed_signup_scroll">
+					
+					<input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required>
+					<!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+					<div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_06562c29028f6e0751eee5238_d5c5306a2e" tabindex="-1" value=""></div>
+					<div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+					</div>
+				</form>
+				</div>
+				<!--End mc_embed_signup-->
 				<p class="copyright text-muted">No Copyright &copy; Weekly Music Thread 2020</p>
 			</div>
 		</div>
