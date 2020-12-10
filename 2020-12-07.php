61771b1392d3143d7b696946e79c602319948c9b
Sam <sam.paderewski@gmail.com>
2020-12-10 17:04:04 -0500

    new post

---
+++ 2020-12-07.php
@@ -0,0 +1,121 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+  
+  <link rel="icon" type="image/x-icon" href="img/favicon.ico" />
+
+  <title>Weekly Music Thread</title>
+
+  <!-- Bootstrap core CSS -->
+  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Custom fonts for this template -->
+  <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+  <link href='https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
+  <link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
+
+  <!-- Custom styles for this template -->
+  <link href="css/clean-blog.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+  <?php include('php/navbar.php'); ?>  
+
+  <!-- Page Header -->
+  <header class="masthead" style="background-image: url('img/vaporheader.png')">
+    <div class="overlay"></div>
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-11 col-md-11 mx-auto">
+          <div class="post-heading">
+            <h1>Hindsight 2020</h1>
+            <h2 class="subheading"></h2>
+            <span class="meta">Posted by
+              <a href="#">DJ Linkin' Project</a>
+              on December 7, 2020</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </header>
+
+  <!-- Post Content -->
+  <article>
+    <div class="container">
+      <div class="row">
+	  
+	  
+	  
+        <div class="col-lg-11 col-md-11 mx-auto" id="postSection">
+         
+          <blockquote class="blockquote">"The Weekly Music Thread was born free, but everywhere it is in 2 Chainz" - An artist formerly known as "Tity Boi" (sic)</blockquote>
+          
+          <p>Well, friends and faithful readers, it's all coming to an end.</p>
+
+          <p>The turn of the calendar from November to December marks the winding down of the year. [Insert obligatory self-aggrandizing comment about 2020 earning some superlative in history and in absolutely no way being as cosmically unremarkable as every other year that we--or anyone else--has ever experienced.] That being said, there was one thing that was special about 2020: the birth of the Weekly Music Thread. </p>
+        
+          <p>And born it was. It was born, and it grew, and it went forth and multiplied. But what of it? What did any of that mean? What was it really worth? What is birth if not the prelude to death? What is a beginning if not a spartan guarantee only of its opposite: an end? Is it simply the year that will end at midnight on December 31st, or something more?</p>
+          
+          <p>Do not be afraid. To paraphrase Angelfire homepages circa 2004, cry not because it's over, but smile because it happened. Alternatively, perhaps our readers prefer 1998 one-hit-wonder Semisonic: every new beginning comes from some other beginning's end. Prescient, isn't it? Because the truth is, folks, this is, in some ways, the beginning of the end of the Weekly Music Thread as you know it. (Of course, that being said, it's also, according to the same, aforementioned transitive property of "Closing Time," the beginning of the end <i>of the beginning</i> of the Weekly Music Thread as you know it...try to keep up.)</p>
+
+          <p>What are we talking about? Nevermind that now (via Kirk Cobain). Because all you need to know for now is this: it's time to start compiling your year-end lists. Specifically, we're looking for two different lists from each of you--your 10 favorite songs of the year, and your 10 favorite albums of the year (In order, if rankings are your sort of thing.) Now, keep in mind that we here at the WMT choose our words wisely, which is why we said favorite and not best. We're not into joyless taxonomies and pseudoscientific 10.0 critical scales. These lists are just about making a record of the things from 2020 you never want to forget. Kinda like The Lincoln Project, but for heady nuggz.</p>
+          
+          <p>You've got the month to prepare your entries. Instructions to follow in the <b>Audience participation round</b> below.</p>
+                    
+          <h2 class="section-heading">And now, onto the show...</h2>
+          <br>
+        </div>
+
+        <div class="col-lg-11 col-md-11 mx-auto"> <!-- From The Vault/We're Stil Listening/Up The Punx-->
+         
+          <hr>
+
+          <h2 class="section-heading" style="font-size:125%">From the Vault</h2>
+
+          <p>We were engaging in some socially proximal, indoor dining this weekend when over the speakers came a song we hadn't heard in some time: Future Islands' "Seasons (Waiting on You)." Probably the most meaningful track of 2014, this one blew the doors off when it went <a href="https://www.youtube.com/watch?v=t9tXcYkL6Nw">"live on Letterman"</a>. One of the rare times a late performance elevated an act for purely aesthetic reasons, you simply have to see this performance. As Dave himself said enthusiastically in the aftermath, we'll "take all of that you got."</p>
+
+          <h2 class="section-heading" style="font-size:125%">We're Still Listening To</h2>
+
+          <p>The Ergs!, Kitten</p>
+
+          <h2 class="section-heading" style="font-size:125%;color: rgb(142, 47, 231);">Audience Participation Round</h2>
+
+          <p>Here's the plan, dear readers. Compile your "best of 2020" lists. We're suggesting 10-item lists for songs and albums, ranked or not, but feel free to interpret the instructions as you see fit. You've got until the turn of the New Year to submit your submissions, along with a WMT-worthy, untraceable anonymous DJ pseudonym (you know, in the spirit of things). These will then be integrated into a larger whole and disseminated back to you as part of something that none of you see coming.</p>
+
+         </div>
+        
+		
+		
+		
+      </div>
+    </div>
+  </article>
+
+  <hr>
+
+  <?php include('php/footer.php'); ?>  
+
+  <!-- Bootstrap core JavaScript -->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  <!-- Custom scripts for this template -->
+  <script src="js/clean-blog.js"></script>
+  
+  <script src="js/parse_music_entries.js"></script>
+  
+  <script>
+	displayPosts('2020-12-07');
+  </script>
+
+</body>
+
+</html>
