7e3a0cc8b9c42759f93e967835669ec142d5a284
Sam <sam.paderewski@gmail.com>
2020-12-10 18:52:50 -0500

    all entries page test

---
+++ js/list_all_music_entries.js
@@ -0,0 +1,43 @@
+function displayAll() {
+	const queryString = window.location.search;
+	const urlParams = new URLSearchParams(queryString)
+	const searchedArtist = urlParams.get('artist')
+	console.log(searchedArtist)
+
+	$("#postHeading").append('<h1>'+searchedArtist+'</h1>');
+
+	var postCounter = 1;
+	var i;
+	
+	// jquery function to get the json file and then put it into memory as "postData" var
+	$.getJSON('https://weeklymusicthread.com/music_entries.json', postData => {
+	
+
+		for (i = 0; i < 10; i++) {
+
+			var postId = 'post'+ postCounter;
+			$("#postSection").append('<div class="post-container" id="'+postId+'"></div>');
+
+			if (entry.spotify_id != null)  {
+				console.log('contains spotify ID: '+postData[i].spotify_id);
+				if (entry.song != null) {
+					$("#"+postId).append('<p><b> "'+postData[i].song+''+'" by '+ '<a href="search.html?artist='+postData[i].artist+'">"'+postData[i].artist+'"</a>'+' </b>' + '&nbsp; <span style = "color:rgb(180, 180, 180)">(posted on '+postData[i].post_page + ')</span></p>');
+					$("#"+postId).append('<div style="float: left; clear: left; padding-right: 2em;"><iframe src="https://open.spotify.com/embed/track/'+postData[i].spotify_id+'" width="300" height="380" scrolling="no" frameborder="0" float="right" allowtransparency="true" allow="encrypted-media"></iframe></div>');
+				} else {
+					console.log("Song Null");
+					$("#"+postId).append('<p><b><i> '+postData[i].album+''+'</i> by '+ '<a href="search.html?artist='+postData[i].artist+'">"'+postData[i].artist+'"</a>'+' </b>' + '&nbsp; <span style = "color:rgb(180, 180, 180)">(posted on '+postData[i].post_page + ')</span></p>');
+					$("#"+postId).append('<div style="float: left; clear: left; padding-right: 2em;"><iframe src="https://open.spotify.com/embed/album/'+postData[i].spotify_id+'" width="300" height="380" scrolling="no" frameborder="0" float="right" allowtransparency="true" allow="encrypted-media"></iframe></div>');
+				}
+			}
+
+			postData[i].description_lines.forEach (descLine => {
+				$("#"+postId).append('<p>'+descLine+'</p>');
+			});
+			
+			$("#postSection").append('<br/>');
+			
+			postCounter++;
+		}
+	
+	});
+}
---
+++ music_entries.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+  
+  <link rel="icon" type="image/x-icon" href="img/favicon.ico" />
+
+  <title>Weekly Music Thread</title>
+
+  <!-- Bootstrap core CSS -->
+  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Custom fonts for this template -->
+  <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+  <link href='https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
+  <link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
+
+  <!-- Custom styles for this template -->
+  <link href="css/clean-blog.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+  <?php include('php/navbar.php'); ?>  
+
+  <!-- Page Header -->
+  <header class="masthead" style="background-image: url('img/vaporheader.png')">
+    <div class="overlay"></div>
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-11 col-md-11 mx-auto">
+          <div class="post-heading">
+            <h1>Just The Music</h1>
+          </div>
+        </div>
+      </div>
+    </div>
+  </header>
+
+  <!-- Post Content -->
+  <article>
+    <div class="container">
+      <div class="row">
+       <div class="col-lg-11 col-md-11 mx-auto" id="postSection">
+         
+       </div>
+      </div>
+    </div>
+  </article>
+
+  <hr>
+
+  <?php include('php/footer.php'); ?>  
+
+  <!-- Bootstrap core JavaScript -->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  <!-- Custom scripts for this template -->
+  <script src="js/clean-blog.js"></script>
+  
+  <script src="js/list_all_music_entries.js"></script>
+  
+  <script>
+	displayAll();
+  </script>
+
+</body>
+
+</html>
