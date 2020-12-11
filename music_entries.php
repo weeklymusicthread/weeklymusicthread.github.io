cc359a04e882f5892488388f9f5b4c108b3de726
Sam <sam.paderewski@gmail.com>
2020-12-10 19:18:13 -0500

    all entries page test

--- music_entries.php
+++ music_entries.php
@@ -63,9 +63,9 @@
   <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
   <!-- Custom scripts for this template -->
-  <script src="js/clean-blog.js"></script>
+  <script src="js/clean-blog.js?uid=<?php echo uniqid(); ?>"></script>
   
-  <script src="js/list_all_music_entries.js"></script>
+  <script src="js/list_all_music_entries.js?uid=<?php echo uniqid(); ?>"></script>
   
   <script>
 	displayAll();
