b1b34abb200c8d4c2902977a8e72ffe501bb9e14
Sam <sam.paderewski@gmail.com>
2020-12-13 12:57:13 -0500

    center Navbar

--- php/navbar.php
+++ php/navbar.php
@@ -6,7 +6,7 @@
 			Menu
 			<i class="fas fa-bars"></i>
 		</button>
-		<div class="collapse navbar-collapse text-center" id="navbarResponsive">
+		<div class="collapse navbar-collapse" id="navbarResponsive">
 			<ul class="navbar-nav ml-auto">
 				<li class="nav-item">
 					<a class="nav-link" href="index.php">Home</a>
