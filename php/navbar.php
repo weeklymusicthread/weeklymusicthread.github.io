99b9fb8abd4f43bdb140b580228d9bde034fdc8d
Sam <sam.paderewski@gmail.com>
2020-12-13 13:28:44 -0500

    center Navbar

--- php/navbar.php
+++ php/navbar.php
@@ -32,7 +32,7 @@
 				</li>
 			</ul>
 		</div>
-		<form class="form-inline d-flex justify-content-center md-form form-sm">
+		<form class="form-inline d-flex justify-content-right md-form form-sm">
   			<input class="form-control form-control-sm mr-3 w-75" type="text" placeholder="Search"aria-label="Search">
   			<i class="fas fa-search" aria-hidden="true"></i>
 		</form>
