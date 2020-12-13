dc795c87840e68cda33cfa6e28d4b6804c82e7e6
Sam <sam.paderewski@gmail.com>
2020-12-13 13:48:52 -0500

    undo nav changes

--- php/navbar.php
+++ php/navbar.php
@@ -1,13 +1,13 @@
 <!-- Navigation -->
 <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
 	<div class="container">
-		<!-- <a class="navbar-brand" href="index.php">Weekly Music Thread</a> -->
+		<a class="navbar-brand" href="index.php">Weekly Music Thread</a>
 		<button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
 			Menu
 			<i class="fas fa-bars"></i>
 		</button>
-		<div class="collapse navbar-collapse w-100" id="navbarResponsive">
-			<ul class="navbar-nav w-100 justify-content-center">
+		<div class="collapse navbar-collapse" id="navbarResponsive">
+			<ul class="navbar-nav ml-auto">
 				<li class="nav-item">
 					<a class="nav-link" href="index.php">Home</a>
 				</li>
