0239529c7507306988c7426da3b9cf2d292c4bea
Sam <sam.paderewski@gmail.com>
2020-12-14 19:39:19 -0500

    autocomplete search bar

--- js/search-bar.js
+++ js/search-bar.js
@@ -1,10 +1,11 @@
 // search bar
 
+var searchTerms = ["Beach Bunny"];
+
 function autocomplete(inp, arr) {
   /*the autocomplete function takes two arguments,
   the text field element and an array of possible autocompleted values:*/
   var currentFocus;
-  var searchTerms;
 
   /*load up the autocomplete suggestion array*/
   $.getJSON('https://weeklymusicthread.com/music_entries.json', postData => {
