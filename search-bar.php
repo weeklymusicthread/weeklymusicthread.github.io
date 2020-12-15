5e4f9ff5d8d53755309641a7fec62c37093ec585
Sam <sam.paderewski@gmail.com>
2020-12-14 19:14:45 -0500

    autocomplete search bar

---
+++ search-bar.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link href="css/search-bar.css?<?php echo time(); ?>" rel="stylesheet">
+</head>     
+<body>
+
+<h2>Autocomplete</h2>
+
+<p>Start typing:</p>
+
+<!--Make sure the form has the autocomplete function switched off:-->
+<form autocomplete="off" action="/search.php">
+  <div class="autocomplete" style="width:300px;">
+    <input id="myInput" type="text" name="artist" placeholder="Search Artists">
+  </div>
+  <input type="submit">
+</form>
+
+
+<script src="js/search-bar.js?uid=<?php echo uniqid(); ?>"></script>
+
+</body>
+</html>
