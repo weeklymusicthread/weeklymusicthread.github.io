03471d4a36777ef2082f0400aa045ddbdfd706bb
Sam <sam.paderewski@gmail.com>
2020-12-21 09:30:22 -0500

    Revert "no search results"
    This reverts commit d4968cc3eb37cd563be123899786faa67a349639.

--- index.php
+++ index.php
@@ -11,18 +11,8 @@
   <link rel="icon" type="image/x-icon" href="img/favicon.ico" />
 
   <title>Weekly Music Thread</title>
-
-  <!-- Bootstrap core CSS -->
-  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-
-  <!-- Custom fonts for this template -->
-  <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
-  <link href='https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
-  <link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
-
-  <!-- Custom styles for this template -->
-  <link href="css/clean-blog.css?<?php echo time(); ?>" rel="stylesheet">
-  <link href="css/search-bar.css?<?php echo time(); ?>" rel="stylesheet">
+	
+	<?php include('php/header.php'); ?>
 
 </head>
 
@@ -63,15 +53,7 @@
 
   <?php include('php/footer.php'); ?>
 
-  <!-- Bootstrap core JavaScript -->
-  <script src="vendor/jquery/jquery.min.js"></script>
-  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-
-  <!-- Custom scripts for this template -->
-  <script src="js/clean-blog.js"></script>
-
   <script src="js/parse_post_previews.js?uid=<?php echo uniqid(); ?>"></script>
-  <script src="js/search-bar.js?uid=<?php echo uniqid(); ?>"></script>
   
 
   <script> displayPreviews();  </script>
--- js/parse_search.js
+++ js/parse_search.js
@@ -8,7 +8,7 @@ function searchName() {
 
 	var postCounter = 1;
 	var artistMatch = false;
-	var pageTitle = "No Result Found for " + searchedArtist;
+	var pageTitle;
 	var firstMatch = true;
 	
 	// jquery function to get the json file and then put it into memory as "postData" var
