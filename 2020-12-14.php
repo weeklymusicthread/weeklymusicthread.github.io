a88c6e7bbf08601e5c7762907d94f85b3d196b6b
Sam <sam.paderewski@gmail.com>
2020-12-22 11:16:30 -0500

    new post

---
+++ 2020-12-14.php
@@ -0,0 +1,125 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+  
+  <link rel="icon" type="image/x-icon" href="img/favicon.ico" />
+
+  <title>Weekly Music Thread</title>
+
+  <!-- Bootstrap core CSS -->
+  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Custom fonts for this template -->
+  <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+  <link href='https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
+  <link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
+
+  <!-- Custom styles for this template -->
+  <link href="css/clean-blog.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+  <?php include('php/navbar.php'); ?>  
+
+  <!-- Page Header -->
+  <header class="masthead" style="background-image: url('img/vaporheader.png')">
+    <div class="overlay"></div>
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-11 col-md-11 mx-auto">
+          <div class="post-heading">
+            <h1>Bubble Boy Band</h1>
+            <h2 class="subheading"></h2>
+            <span class="meta">Posted by
+              <a href="#">DJ Liv "Like That" Tyler</a>
+              on December 14, 2020</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </header>
+
+  <!-- Post Content -->
+  <article>
+    <div class="container">
+      <div class="row">
+	  
+	  
+	  
+        <div class="col-lg-11 col-md-11 mx-auto" id="postSection">
+         
+          <blockquote class="blockquote">"I'd rather make a movie about a guy walking his dog than about the emperor of China" - Christopher Nolan</blockquote>
+          
+          <p>As we're sure you're all aware, the year is winding to a close. And, as such, we decided it would be a good idea to get our affairs in order. You know, taking out insurance policies, investing in Dow Jones' industrial assets, and swinging by our local Ed.D. for our annual physical exam.</p> 
+          
+          <p>It was in the pursuit of this lattermost endeavor that something compelling happened. And because compelling somethings so rarely happen--especially to us--we thought it fit to share with you, our loyal readers, here. We arrived at the DOCTOR's office (get used to it), took a number, and sat down. Minutes passed, a quarter hour, a half hour. Aside from the idle small talk that came along with every patient as they checked in, there was no conversation in the waiting room. Throughout this peaceful time, a man and a woman who were presumably married or otherwise together, sat silently about six feet across from us. (His name is DOCTOR Fauci--get used to it.) They hadn't spoken at all, neither to one another nor anyone else, and we hadn't noticed much about them. They had seemed unremarkable. As it turned out, they were not.</p>
+          
+          <p>That's because, after thirty minutes of idle silence and without even using a cell phone--really just absentmindedly staring at the floor--the man cleared his voice and spoke. "Seen that movie <i>Bubble Boy</i>," he offered, in an intersection of construction and delivery which felt perfectly ambiguous. Was this a question? A declaration? His tone gave away nothing, and neither did his demeanor. On top of that, the--comment? question? utterance?--seemed as though it had been directed toward nobody in particular. Certainly neither his companion nor any of the other patients in the waiting room so much as acknowledged that the interminable silence had just been broken (and by a comment about the 2001 Jake Gyllenhaal vehicle <i>Bubble Boy</i>--which did for severe combined immunodeficiency (or SCID) what <i>Philadelphia</i> did for HIV/AIDS--no less.)</p>
+          
+          <p>"Stoopit," the man went on, shaking his head in dismissive rebuke. "Corny as hell." We looked around. If anyone else was hearing this, they gave no indication of it.</p>
+          
+          <p>"Still," the man piped up again one final time. This time, he lifted his head to look around the room, one of his eyebrows raised. He showed neither the flat affect nor the naked condescension he had worn just moments earlier. His tone was now...elegiac. "He lived like that. That was how he lived." And with that, he returned his gaze to the floor and was silent again.</p>
+
+          <p>We were flabbergasted. What could this have been? A film review? A commentary on the prescience of <i>Bubble Boy,</i> vis-a-vis the masking, hand sanitizing, social distancing fads that would come into fashion almost twenty years later? Are we all living like that? Is that how we're living? And was he protesting that? Was this man an anti-masker? Was he an anti-vaxxer? A science denier? A Tea Party-goer? An InfoWarrior? A Proud and/or Boogaloo Bo(y/i)? Was this a Q drop? We had so many questions, but looking at the man then, it was clear that he had nothing more to offer. He had said his piece, and had returned to his resting state, his own sort of reactionary Bubble Boy. God bless.</p>
+
+          <h2 class="section-heading">And now, onto the show...</h2>
+          <br>
+        </div>
+
+        <div class="col-lg-11 col-md-11 mx-auto"> <!-- From The Vault/We're Stil Listening/Up The Punx-->
+         
+          <hr>
+
+          <h2 class="section-heading" style="font-size:125%">From the Vault</h2>
+
+          <p>Taylor Swift released a new record. Although we haven't had time to listen to it, we understand that it inhabits the same, uncapitalized universe as folklore. That, plus the Dylan, got us listening to some other folk-adjacent music, including The Tallest Man on Earth. We've been revisiting <i>The Wild Hunt</i> (2010) as well as several tracks off of <i>Dark Bird is Home</i> (2016), most notably "Slow Dance." Although Kristian Matsson is an excellent guitar player--he experiments with lots of alternative tunings and makes severely complicated fingerpicking parts look easy--his real allure lies in two areas: 1) his roughly hewn voice, and 2) the poetry of his lyrics. First, the voice. It's not pretty, which is kind of what makes it, paradoxically, so welcoming. When he sings softly, he sounds almost like someone you know. And when he belts, you really feel all of the strain, which comes as a sort of proxy for the emotion you imagine he's feeling. Now, a lot of folk music is essentially just pleasant melodies which allow the lyrics to do the heavy lifting, and rather than saying that this is no different, we're going to say that this is actually an exalted example of that. When Matsson sings things like "Oh, but rumor has it that I wasn't born/I just walked in one frosty morn/Into the vision of some vacant mind," we feel as though, well, neither were we born. (And, in some sense, this is true, isn't it? No one, after all, can access the experience of their own birth.) Or on the aforementioned "Slow Dance," in which Matsson laments his life on the road and what it is doing to his marriage back home, he sings "And now I hold on fall is greeting me to stay/And we slow dance in the kitchen and I dream the days away, oh" so swiftly that you almost miss it. But after the first time you do notice it, you can never listen to the song again without feeling fully that line's deep poignancy. (And if that hits hard, just wait until the final verse: "In this sunlight I just can't believe your face/We're some laundry line believers/We're just kids in many ways, oh/And you smell like smoke and honey in my arms/I'm somewhere in some falling and I'm learning of your language for to say/In times like these I should never grow away." The language is so simple, and yet (or perhaps it's 'and so'?) it captures so effectively such a complicated mixture of love, longing, guilt, regret--and all with meter and rhyme, from someone who isn't even a native English speaker. Eat your heart out, Louise Gluck.)</p>
+
+          <h2 class="section-heading" style="font-size:125%">We're Still Listening To</h2>
+
+          <p>Future Islands, Tove Styrke, Sincere Engineer</p>
+
+          <h2 class="section-heading" style="font-size:125%">Up The Punx</h2>
+
+          <p>This week, as we bring you another dubious selection for the genre and our punk credentials suffer even further, we present Bully's <i>SUGAREGG</i> (2020). For fans of Charly Bliss and Sincere Engineer, this kind of sounds like if those two bands met up and made a record about their bipolar disorder. (Maybe sprinkle a touch of White Lung in as well?) This is raw, messy stuff--the kind of record that really would've been ruined with over-production, but there's just enough distortion on the instrumentals and rust on Alicia Bognanno's voice to capture the sort of sentiments that the album's lyrics put forward.</p>
+
+          <h2 class="section-heading" style="font-size:125%;color: rgb(142, 47, 231);">Audience Participation Round</h2>
+
+          <p>Entertain us with your greatest "overheard in public" stories! Bonus points if they come in the form, like this one, of impromptu film criticism and sociopolitical commentary. Oh, and get those Best of 2020 lists in.</p>
+
+         </div>
+        
+		
+		
+		
+      </div>
+    </div>
+  </article>
+
+  <hr>
+
+  <?php include('php/footer.php'); ?>  
+
+  <!-- Bootstrap core JavaScript -->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  <!-- Custom scripts for this template -->
+  <script src="js/clean-blog.js?uid=<?php echo uniqid(); ?>"></script>
+  
+  <script src="js/parse_music_entries.js?uid=<?php echo uniqid(); ?>"></script>
+  
+  <script>
+	displayPosts('2020-12-07');
+  </script>
+
+</body>
+
+</html>
