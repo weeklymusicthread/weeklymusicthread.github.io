26cfbe3528630032364f97d57f2a78e6cb39a022
Sam <sam.paderewski@gmail.com>
2020-12-22 11:45:23 -0500

    new post

--- 2020-12-21.php
+++ 2020-12-21.php
@@ -42,36 +42,36 @@
   <article>
     <div class="container">
       <div class="row">
-	  
-	  
-	  
+	  	  
         <div class="col-lg-11 col-md-11 mx-auto" id="postSection">
          
           <blockquote class="blockquote">"I don't try to guess what a million people will like. I try to guess what 70 million people will like." - Barack Obama, populist film critic</blockquote>
           
           <p>Boy, this week, have we got a special treat! We bring you: a year-end review of Barack Obama's year-end reviews! (For brevity's sake, we've chosen to review Obama's 2018-2020 movie lists.)</p> 
           
-          <p>Obama first showed up on the year-end best-of movie list scene in 2018, when he posted a rough but auspicious debut to a little known social networking site called Facebook. That list opens strong with a number of recognizable bangers: <i>Annihilation<i>, <i>Black Panther</i>, and <i>BlacKkKlansman</i>. Any reader of year-end lists will recognize these, and they do their job well, announcing that although you might not be familiar with this fresh new movie reviewer or his taste in film, he's going to give you recommendations you're already well familiar with. From there, the reviewer takes a further dive into race with <i>Blindspotting</i>, which, along with <i>If Beale Street Could Talk</i>, heralds one of the things that really set this newcomer apart in 2018 movie reviews: an inclination to pay special attention to films that focus on the otherwise totally neglected issue of race. Readers of lists written for viewers will also notice the very tasteful inclusion of foreign language films such as <i>Burning</i>, <i>Roma</i>, and <i>Shoplifters</i>. "Hey," these inclusions say to the audience, "I seem worldly. Share this list breathlessly with your friends, and you'll seem worldly, too!" America's not all that's out there, after all--call your host moms and play some bloody footie once in a while! <i>The Death of Stalin</i> tells us, like, hey, this guy can have a good time, and <i>Eighth Grade</i> shows that he's down to Earth and that he's got a soft spot, too. Sprinkle in a little Americana with <i>The Rider</i> and <i>Won't You Be My Neighbor</i>, a nod to the troops with <i>Leave No Trace</i>, and the female-forward <i>Support the Girls</i>, and this hot new upstart has got himself a well-rounded and perfectly tasteful best-of debut that is COVID-prescient in its degree of sanitization. It's designed to impress everyone and offend no one. It's almost as though it was made by a committee.</p>
+          <p>Obama first showed up on the year-end best-of movie list scene in 2018, when he posted a rough but auspicious debut to a little known social networking site called Facebook. That list opens strong with a number of recognizable bangers: <i>Annihilation</i>, <i>Black Panther</i>, and <i>BlacKkKlansman</i>. Any reader of year-end lists will recognize these, and they do their job well, announcing that although you might not be familiar with this fresh new movie reviewer or his taste in film, he's going to give you recommendations you're already well familiar with. From there, the reviewer takes a further dive into race with <i>Blindspotting</i>, which, along with <i>If Beale Street Could Talk</i>, heralds one of the things that really set this newcomer apart in 2018 movie reviews: an inclination to pay special attention to films that focus on the otherwise totally neglected issue of race. Readers of lists written for viewers will also notice the very tasteful inclusion of foreign language films such as <i>Burning</i>, <i>Roma</i>, and <i>Shoplifters</i>. "Hey," these inclusions say to the audience, "I seem worldly. Share this list breathlessly with your friends, and you'll seem worldly, too!" America's not all that's out there, after all--call your host moms and play some bloody footie once in a while! <i>The Death of Stalin</i> tells us, like, hey, this guy can have a good time, and <i>Eighth Grade</i> shows that he's down to Earth and that he's got a soft spot, too. Sprinkle in a little Americana with <i>The Rider</i> and <i>Won't You Be My Neighbor</i>, a nod to the troops with <i>Leave No Trace</i>, and the female-forward <i>Support the Girls</i>, and this hot new upstart has got himself a well-rounded and perfectly tasteful best-of debut that is COVID-prescient in its degree of sanitization. It's designed to impress everyone and offend no one. It's almost as though it was made by a committee.</p>
           
           <p>Of course, debuts are a dime a dozen, and the entire film review community was wondering if this early new star would shine again in 2019. Well, results were mixed. Obama was back with some of the early signatures he had first scrawled a year earlier, such as odd numbers and inexplicable alphabetization. In 2019, that alphabetization was heavily front-loaded, with nearly half the list finished by the time the letter "D" showed up. It's almost as though some intern had been ordered to filter through all of the movies that year and generate a best-of list for our reviewer, and didn't cover their tracks that well. Weird. At any rate, although this list was somehow both more AND less of the same, it basically saved itself with the appearance of a last-ditch post-script: "And a quick list of TV shows that I considered as powerful as movies." This sent shockwaves through critical circles. Never before had a critic considered that TV shows--which are weak, flimsy things--could possibly approach the sheer level of power of 2019 inclusions such as <i>Booksmart</i> or <i>Little Women</i>, but this is where movie critics like Barack Obama break new ground. Just which shows were these? <i>Fleabag</i> (but, as Obama specified, only season two), a Toni Colette minisery, and the doomed first season of <i>Watchmen</i>. Although what had come before probably didn't measure up to 2018, the 2019 coda firmly established Obama as exactly the radical that some people feared he would be.</p>
           
           <p>And now for this year. 2020. Obligatory comment about our lives, coronavirus, we're all in this together. Well, not Obama, because if you thought you were going to get comfort and consistency from him, you were sadly mistaken. Without major theater releases to lean on, our reviewer had to come up with some other way to make a perfectly neutered, sexless list of pop culture recommendations. And so how did he do it? Apparently, by scrolling multiple times through some alphabetized list of the movies that were released in 2020. The list starts off with <i>Ma Rainey's Black Bottom</i>--perhaps the obvious choice either due to lead Chadwick Boseman's death and/or the fact that it came out on November 25th, and was therefore probably fresh in the reviewer's mind. But seriously, after that, it really does just look like he scrolled through the alphabet a few times:</p>
 
-          <p><i>- Beanpole<br>- Bacurau<br>- Nomadland<br>- Soul<br>- Small Axe: Lover's rock</i></p>
+          <div style = "padding-left: 5em">
+            <p><i>- Beanpole<br>- Bacurau<br>- Nomadland<br>- Soul<br>- Small Axe: Lover's rock</i></p>
 
-          <p>("Oh shit, I'm already through "S" and I only have five movies...better go back to the beginning")</p>
+            <p>("Oh shit, I'm already through "S" and I only have five movies...better go back to the beginning")</p>
 
-          <p><i>- Mank<br>- Martin Eden<br>- Let Him Go<br>- Time</i></p>
+            <p><i>- Mank<br>- Martin Eden<br>- Let Him Go<br>- Time</i></p>
 
-          <p>("Fuuuuck dude. Okay one more time through")</p>
+            <p>("Fuuuuck dude. Okay one more time through")</p>
 
-          <p><i>- Boys State<br>- Selah and the State</i></p>
+            <p><i>- Boys State<br>- Selah and the State</i></p>
 
-          <p>("Last time, swear to God")<p>
+            <p>("Last time, swear to God")<p>
 
-          <p><i>- Crip Camp: A Disability Revolution</i></p>
+            <p><i>- Crip Camp: A Disability Revolution</i></p>
 
-          <p>("Good enough")<p>
+            <p>("Good enough")<p>
+          </div>
 
           <p>Now, one might think that this would not inspire confidence. No one looks good here--neither the intern who wrote this list, nor the former hope/change enthusiast who selected that intern for the job. And yet, the community seemed to accept it without much of a fight. Perhaps that's because Obama as film reviewer doesn't necessarily play fair. (If your friendly neighborhood WMT winds up, as a result of this whisteblowing, prosecuted under the 1917 Espionage Act, you'll know why!) At any rate, we'll wait to see what next year brings. (If we were betting men, we'd put our money on a list that starts with a movie whose title is composed exclusively of special characters, then includes a couple of titles made up of numerals, and ends with a film called <i>Aardvark</i>. Or maybe just <i>Aaaaaaaaa</i>. There are gonna be a lot of 2020 retrospectives to fit in the middle of that list, after all.),/p>
 
