ca4eabb371c45722e723c56eb26e2cb94ac38ddf
Sam <sam.paderewski@gmail.com>
2021-01-04 12:32:43 -0500

    2020Lists Page

--- 2020Lists.php
+++ 2020Lists.php
@@ -63,11 +63,11 @@
         <p><b>DJ Bestivus: Best of Lists for the Rest of Us aka The WMT Editors</b></p>
       </div>
       <div class="row">
-       <div class="col-lg-11 col-md-11 mx-auto" id="postSection">
+       <div class="col-lg-8 col-md-8 mx-auto" id="postSection">
 
           <div class="row justify-content-center">
             <p><b>DJ Bestivus: Best of Lists for the Rest of Us aka The WMT Editors</b></p>
-                <div class="column justify-content-center">
+                <div class="column text-center">
                   <p><u>Albums</u></p>
                 </div>
                 <div class="column text-center">
@@ -77,29 +77,27 @@
 
           <div class="row">
               <div class="column">
-                <p><u>Albums</u></p>
-          
-                <p>1. <i>Truth or Consequences</i> by Yumi Zouma</p>
-                <p>2. <i>Rough and Rowdy Ways</i> by Bob Dylan</p>
-                <p>3. <i>Honeymoon</i> by Beach Bunny</p>
-                <p>4. <i>Drug Dealing is a Lost Art</i> by Beach Bunny</p>
-                <p>5. <i>Pop Music / False B Sides II</i> by Baths</p>
+                
+                 <p>1. <i>Truth or Consequences</i> by Yumi Zouma<br>
+                    2. <i>Rough and Rowdy Ways</i> by Bob Dylan<br>
+                    3. <i>Honeymoon</i> by Beach Bunny<br>
+                    4. <i>Drug Dealing is a Lost Art</i> by Beach Bunny<br>
+                    5. <i>Pop Music / False B Sides II</i> by Baths</p>
                 
               </div>
 
               <div class="column">
-                <p><u>Songs</u></p>
-                
-                <p>1. Murder Most Foul by Bob Dylan</p>
-                <p>2. (Un)knowing by Young Jesus</p>
-                <p>3. RASCAL by RMR</p>
-                <p>4. Pink Pony Club by Chappell Roan</p>
-                <p>5. Lonely After by Yumi Zouma</p>
-                <p>5. Cool for a Second by Yumi Zouma</p>
-                <p>5. Southwark by Yumi Zouma</p>
-                <p>8. Ms. California by Beach Bunny</p>
-                <p>9. Camouflage by the Front Bottoms</p>
-                <p>10. Darkness by Katie Dey</p>
+            
+                 <p>1. Murder Most Foul by Bob Dylan<br>
+                    2. (Un)knowing by Young Jesus<br>
+                    3. RASCAL by RMR<br>
+                    4. Pink Pony Club by Chappell Roan<br>
+                    5. Lonely After by Yumi Zouma<br>
+                    5. Cool for a Second by Yumi Zouma<br>
+                    5. Southwark by Yumi Zouma<br>
+                    8. Ms. California by Beach Bunny<br>
+                    9. Camouflage by the Front Bottoms<br>
+                    10. Darkness by Katie Dey</p>
 
               </div>
           </div>         
