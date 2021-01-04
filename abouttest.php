e7ca31e3b66d535487748cdc5666177eceff9565
Sam <sam.paderewski@gmail.com>
2021-01-03 19:03:12 -0500

    new contact and about us

--- abouttest.php
+++ abouttest.php
@@ -21,15 +21,35 @@
   <?php include('php/navbar.php'); ?>
 
   <!-- Page Header -->
-  <header class="masthead" style="background-image: url('img/about-bg.jpg')">
+  <header class="masthead">
     <div class="overlay"></div>
     <div class="container">
       <div class="row">
         <div class="col-lg-8 col-md-10 mx-auto">
-          <div class="page-heading">
-            <h1>About Us</h1>
-            <span class="subheading"></span>
-          </div>
+        <div class="row">
+              <div class="column">
+                <p>Well she said it's cold outside and she hands me my raincoat 
+                <br>She's always worried 'bout things like that
+                <br>Well, she said it's all gonna end and it might as well be my fault
+                <br>And she only sleeps when it's rainin'
+                <br>And she screams
+                <br>And her voice is strainin'
+                <br>She says 'baby, it's 3 AM; I must be lonely'
+                <br>When she says 'baby,' I can't help but be scared of it all sometimes
+                <br>And the rain's gonna wash away, I believe it </p>
+              </div>
+              <div class="column">
+                <p>She puts it up his nose and tells him order more bottles 
+                <br>He's never worried 'bout things like that
+                <br>She knows his credit card PIN but that's really his fault
+                <br>And she'll only dance when it's rainin'
+                <br>And he'll scream
+                <br>And she'll hear his voice
+                <br>And he says 'baby, it's 3 AM and I'm fuckin' horny'
+                <br>And he say's 'baby, I got a thousand dollars, all straight cash
+                <br>I've made it rain, now when are we leavin'</p>
+              </div>
+            </div>
         </div>
       </div>
     </div>
