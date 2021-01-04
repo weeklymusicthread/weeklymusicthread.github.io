78ed3ce132070b4e4ab7dc4b204f6cbcf7ecb4bf
Sam <sam.paderewski@gmail.com>
2021-01-03 21:49:55 -0500

    new contact and about us

---
+++ 2020-12-28.php
@@ -0,0 +1,130 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+  
+  <link rel="icon" type="image/x-icon" href="img/favicon.ico" />
+
+  <title>Weekly Music Thread</title>
+
+  <?php include('php/header.php'); ?>
+
+</head>
+
+<body>
+
+  <?php include('php/navbar.php'); ?>  
+
+  <!-- Page Header -->
+  <header class="masthead" style="background-image: url('img/vaporheader.png')">
+    <div class="overlay"></div>
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-11 col-md-11 mx-auto">
+          <div class="post-heading">
+            <h1>Heat Death of the Marvel Cinematic Universe</h1>
+            <h2 class="subheading"></h2>
+            <span class="meta">Posted by
+              <a href="#">DJ Rachel Carson Daly</a>
+              on December 28, 2020</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </header>
+
+  <!-- Post Content -->
+  <article>
+    <div class="container">
+      <div class="row">
+	  	  
+        <div class="col-lg-11 col-md-11 mx-auto" id="postSection">
+         
+          <blockquote class="blockquote">"It's the end of the world as we know it, and I feel fine" - Greta Thunberg </blockquote>
+
+          <p>We were reminded last night of an interview with Colin Firth (<i>In Bruges, The Lobster</i>) in which he said that "whenever you take on playing a villain, he has to cease to be a villain to you. If you judge this man by his time, he's doing very little wrong." Transphobic, cisheteronormative language notwithstanding, this basically means that, like, "I see where he (villain) is coming from."</p> 
+         
+          <p>The quote came back to us as, in preparation for <i>Wonder Woman 1984</i> (aka WW84), we watched the original Gal Gadot <i>Wonder Woman</i> (2017). In that film, the villain is Ares (sort of--you'll see if you watch it), the God of War, who seeks to destroy humanity due to some combination of its general unworthiness as a creation of Zeus and an ecoterrorist revenge plot in response to their poor stewardship of the planet. Gee, where have we heard that before?</p>
+
+          <p>If you think about it, every villain from every movie from the past 5-10 years has been an anti-humanist and at least implicit (often actually explicit) ecoterrorist. Some additional examples:</p>
+
+          <p style = "padding-left: 2em">- Thanos in...<i>Avengers</i> (? I don't actually know what these movies are called)</p>
+
+          <p style = "padding-left: 4em">Thanos is an anti-humanist ecoterrorist who seeks to bring "balance" to the universe by wearing a special (in all likelihood biodegradable) glove that allows him to snap his fingers and prevent overpopulation by killing off half of the universe's living creatures through Green New Deal policies. Thanos: "The universe is finite, its resources, finite. If life is left unchecked, life will cease to exist." (Evidently, Thanos does not follow Neil MiddleName Tyson on Twitter.)</p>
+
+          <p style = "padding-left: 4em">So, how did critics respond?</p>
+
+          <p style = "padding-left: 4em"><a href="https://www.forbes.com/sites/jvchamary/2019/05/07/avengers-endgame-biodiversity/?sh=aa82c29775b4">"The Science of 'Avengers: Endgame' Proves Thanos Did Nothing Wrong"</a></p>
+
+          <p style = "padding-left: 4em"><a href="https://movieweb.com/thanos-infinity-war-main-hero-marvel-movie-universe/">"Why Thanos is Actually the Hero of the MCU"</a></p>
+
+          <p style = "padding-left: 4em"><a href="https://www.vox.com/2018/7/10/17553864/reddit-thanosdidnothingwrong-mass-ban-dusting">"Thanos Did Nothing Wrong"</a> (Who else saw this one coming from Vox??? "Explaining the news" since 2014)</p>
+
+          <p style = "padding-left: 2em">- Erik Killmonger in <i>BlackPpPanther</i></p>
+
+          <p style = "padding-left: 4em">First of all...Erik? We've never seen <i>BlackPpPanther</i>, so we've only heard tell of Killmonger. We always just assumed that this was a mononym, like P!nk or Prince, but it turns out that it's actually a...family name? Perhaps an Irish one? And his first name is...Erik? (This feels a little like finding out that Thanos' first name is, like, Ronald, or something.)</p>
+
+          <p style = "padding-left: 4em">At any rate, EriKkKillmonger isn't so much an ecoterrorist, as far as we can tell, but his desire to exchange one race-based hegemony for another definitely feels antihumanist. And critics definitely identified with it. "I fist-bumped in theater!" <a href="https://www.theatlantic.com/entertainment/archive/2018/02/black-panther-erik-killmonger/553805/">one critic wrote</a>. "IT'S A GOOD IDEA." (Caps Lock, of course, original.) "Killmonger is a role model for black children," another <a href="https://medium.com/@jjjones/erik-killmonger-is-a-role-model-for-black-children-d5da7d46b485">suggested</a>. It's safe to say that Erik O'Kilmonger is a relatable villain.</p>
+
+          <p style = "padding-left: 2em">- Richmond Valentine in <i>Kingsman: The Secret Service</i></p>
+
+          <p style = "padding-left: 4em">Perhaps because it takes itself the least seriously, this is by far the best of the movies to appear on this list. Its villain? Richmond Valentine, a Tom Steyer type billionaire who wants to kill off a large portion of the world's population in order to stop the damage he believes they are doing to--you guessed it--the environment. Valentine was included on SyFy's list of <a href="https://www.syfy.com/syfywire/these-environmentalist-sci-fi-villains-deserved-better">"Environmentalist Sci Fi Villains [That] Deserved Better."</a> Guess who else made that list? That's right...Cornelius Thanos.</p>
+
+          <p>And these are, frankly, just off of the dome. Aquaman and Venom also both feature explicit environmental nutjobs. Christopher Nolan has given us ecoterrorists as well, both explicitly in Batman Begins (R'as al Ghul) and inaudibly in Tenet (the...future? Whoever is fighting the retrograde half of temporal WWIII? Honestly, the foundations of the conflict in this movie are as incomprehensible as the eco-philosophies of the villains listed in this intro.) And, universally, critics love them. (See, for instance, <a href="https://earthbound.report/2020/09/11/tenet-climate-change-and-intergenerational-justice/">"Tenet, Climate Change and Intergenerational Justice,"</a> which, surprisingly, is not a manifesto about sound mixing.)</p>
+
+          <p>You can learn a lot about a time by its movies, and our movies seem to be telling us that people aren't necessarily all that opposed to destroying planets/populations/universes/sound mixes in the name of saving the environment. These are our relatable villains. (Which, in a world in which an environmentalist with a movie villain name like Al Gore can run for president, doesn't actually feel all that far off.)  With characters like these in all of our theaters, it's no wonder Greta Thunberg plays hooky so much!</p>
+
+          <h2 class="section-heading">And now, onto the show...</h2>
+          <br>
+        </div>
+
+        <div class="col-lg-11 col-md-11 mx-auto"> <!-- From The Vault/We're Stil Listening/Up The Punx-->
+         
+          <hr>
+
+          <h2 class="section-heading" style="font-size:125%">From the Vault</h2>
+
+          <p>This week, we bring you a banger from our college days: Hot Chip's "I Feel Better."</p>
+
+          <h2 class="section-heading" style="font-size:125%">We're Still Listening To</h2>
+
+          <p>Yumi Zouma, Heart Bones, Kitten, Young Jesus</p>
+
+          <h2 class="section-heading" style="font-size:125%;color: rgb(142, 47, 231);">Audience Participation Round</h2>
+
+          <p>Remember to get those year-end lists in by midnight on Thursday!</p>
+
+         </div>
+        
+		
+		
+		
+      </div>
+    </div>
+  </article>
+
+  <hr>
+
+  <?php include('php/footer.php'); ?>  
+
+  <!-- Bootstrap core JavaScript -->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  <!-- Custom scripts for this template -->
+  <script src="js/clean-blog.js?uid=<?php echo uniqid(); ?>"></script>
+  
+  <script src="js/parse_music_entries.js?uid=<?php echo uniqid(); ?>"></script>
+  
+  <script>
+	displayPosts('2020-12-28');
+  </script>
+
+</body>
+
+</html>
