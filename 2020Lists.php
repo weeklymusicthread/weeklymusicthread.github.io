1667a4cf073fc738e555e3f5156b79d594a49112
Sam <sam.paderewski@gmail.com>
2021-01-04 14:51:35 -0500

    2020Lists Page

--- 2020Lists.php
+++ 2020Lists.php
@@ -74,13 +74,13 @@
       <div class="row">
        <div class="col-lg-11 col-md-11 mx-auto" id="postSection">
           <div class="row justify-content-center">
-            <div class="column">
-              <p id="djRow">DJ Bestivus: Best of Lists for the Rest of Us<br></p>
-              <p id="djRow2">(aka The WMT Editors)</p>
-              <p> test </p>
-              <p> test </p>
-              <p> test </p>
-            </div>
+            
+            <p id="djRow">DJ Bestivus: Best of Lists for the Rest of Us<br></p>
+            <p id="djRow2">(aka The WMT Editors)</p>
+            <p> test </p>
+            <p> test </p>
+            <p> test </p>
+            
           </div>
           
           <div class="row justify-content-center">
