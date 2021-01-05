90b1dc03b5c1f9d4274e537788ca0247c0861d3f
Alex Page <alexanderkpage@gmail.com>
2021-01-05 12:18:11 -0500

    replicating re-layout across all page content

--- 2020Lists.php
+++ 2020Lists.php
@@ -30,7 +30,7 @@
         margin-bottom: 0px !important;
         margin-top: 0px !important;
         font-size: 125%;
-				text-align: left;
+				text-align: center;
       }
 
       .doublename1 {
@@ -87,7 +87,8 @@
     <div class="container">
       <div class="row">
        <div class="col-lg-10 col-md-10 mx-auto" id="postSection">
-          <div class="row justify-content-center">
+          
+					<div class="row justify-content-center">
             <div class="col-lg-10 col-md-10 mx-auto">
               <p class="doublename1">DJ Bestivus: Best of Lists for the Rest of Us<br></p>
               <p class="doublename2">(aka The WMT Editors)</p>
@@ -164,50 +165,7 @@
 								10. "Sarita Colonia" by <a href="search.html?artist=Sofia Kourtesis">Sofia Kourtesis</a><br>
 							</p>	
 						</div>
-					
-					
-					
-					
-					
-					
-					
-                <div class="column text-center">
-                  <p class="titleRow"><span class="border-bottom">Albums</span></p>
-                </div>
-                <div class="column text-center">
-                  <p class="titleRow"><span class="border-bottom">Songs</span></p>
-                </div>
-          </div>
 
-          <div class="row">
-              <div class="column">
-        
-                 <p>1. <i>Heaven to a Tortured Mind</i> by <a href="search.html?artist=Yves Tumor">Yves Tumor</a><br>
-                    2. <i>What's Your Pleasure?</i> by <a href="search.html?artist=Jessie Ware">Jessie Ware</a><br>
-                    3. <i>Miss Anthropocene</i> by <a href="search.html?artist=Grimes">Grimes</a><br>
-                    4. <i>Punisher</i> by <a href="search.html?artist=Phoebe Bridgers">Phoebe Bridgers</a><br>
-                    5. <i>We Will Always Love You</i> by <a href="search.html?artist=The Avalanches">The Avalanches</a><br>
-                    6. <i>Notes on a Conditional Form</i> by <a href="search.html?artist=The 1975">The 1975</a><br>
-                    7. <i>Truth or Consequences</i> by <a href="search.html?artist=Yumi Zouma">Yumi Zouma</a><br>
-                    8. <i>Honeymoon</i> by <a href="search.html?artist=Beach Bunny">Beach Bunny</a><br>             
-                  </p>
-              </div>
-
-              <div class="column">
-            
-                 <p>1. "Remember Where You Are" by <a href="search.html?artist=Jessie Ware">Jessie Ware</a><br>
-                    2. "Pink Pony Club" by <a href="search.html?artist=Chappell Roan">Chappell Roan</a><br>
-                    3. "Romanticist" by <a href="search.html?artist=Yves Tumor">Yves Tumor</a><br>
-                    4. "The End Is Near" by <a href="search.html?artist=Phoebe Bridgers">Phoebe Bridgers</a><br>
-                    5. "My Name is Dark" by <a href="search.html?artist=Grimes">Grimes</a><br>
-                    6. "If You're Too Shy (Let Me Know)" by <a href="search.html?artist=The 1975">The 1975</a><br>
-                    7. "The Difference" by <a href="search.html?artist=Toro y Moi">Toro y Moi, </a><a href="search.html?artist=Flume">Flume</a><br>
-                    8. "Darkness" by <a href="search.html?artist=Katie Dey">Katie Dey</a><br>
-                    9. "Sweet" by <a href="search.html?artist=Porridge Radio">Porridge Radio</a><br>
-                    10. "Sarita Colonia" by <a href="search.html?artist=Sofia Kourtesis">Sofia Kourtesis</a><br>
-                  </p>
-
-              </div> 
           </div> 
 
           <hr>
@@ -217,48 +175,49 @@
               <p class="djname">DJ Baby Bok Choy<br></p>
             </div>
           </div>
+					
+					
+
+					
+					
+					
           
           <div class="row justify-content-center">
-                <div class="column text-center">
-                  <p class="titleRow"><span class="border-bottom">Albums</span></p>
-                </div>
-                <div class="column text-center">
-                  <p class="titleRow"><span class="border-bottom">Songs</span></p>
-                </div>
-          </div>
-
-          <div class="row">
-              <div class="column">
-        
-                 <p>1. <i>Miss Anthropocene</i> by <a href="search.html?artist=Grimes">Grimes</a><br>
-                    2. <i>Heaven to a Tortured Mind</i> by <a href="search.html?artist=Yves Tumor">Yves Tumor</a><br>
-                    3. <i>What's Your Pleasure?</i> by <a href="search.html?artist=Jessie Ware">Jessie Ware</a><br> 
-                    4. <i>Shall We Go On Sinning So That Grace May Increase?</i> by <a href="search.html?artist=The Soft Pink Truth">The Soft Pink Truth</a><br>
-                    5. <i>Punisher</i> by <a href="search.html?artist=Phoebe Bridgers">Phoebe Bridgers</a><br>
-                    6. <i>Women in Music Pt. III</i> by <a href="search.html?artist=Haim">Haim</a><br>
-                    7. <i>Fetch the Bolt Cutters</i> by <a href="search.html?artist=Fiona Apple">Fiona Apple</a><br>
-                    8. <i>Inner Song</i> by <a href="search.html?artist=Kelly Lee Owens">Kelly Lee Owens</a><br>  
-                    9. <i>Healing Is A Miracle</i> by <a href="search.html?artist=Julianna Barwick">Julianna Barwick</a><br>
-                    10. <i>All The Time</i> by <a href="search.html?artist=Jessy Lanza">Jessy Lanza</a><br>  
-                    Honorable EP Mention: <i>ALIAS</i> by <a href="search.html?artist=Shy Girl">Shy Girl</a><br>  
-                  </p>
-              </div>
-
-              <div class="column">
             
-                 <p>1. "Sarita Colonia" by <a href="search.html?artist=Sofia Kourtesis">Sofia Kourtesis</a><br>
-                    2. "Guilty Conscience" by <a href="search.html?artist=070 Shake">070 Shake</a><br>
-                    3. "Gospel For A New Century" by <a href="search.html?artist=Yves Tumor">Yves Tumor</a><br>
-                    4. "Baby" by <a href="search.html?artist=Four Tet">Four Tet</a><br>
-                    5. "Remember Where You Are" by <a href="search.html?artist=Jessie Ware">Jessie Ware</a><br>
-                    6. "XS" by <a href="search.html?artist=Rina Sawayama">Rina Sawayama</a><br>
-                    7. "Home" by <a href="search.html?artist=Caribou">Caribou</a><br>
-                    8. "For You" by <a href="search.html?artist=India Jordan">India Jordan</a><br>
-                    9. "circle the drain" by <a href="search.html?artist=Soccer Mommy">Soccer Mommy</a><br>
-                    10. "Blinding Lights" by <a href="search.html?artist=The Weeknd">The Weeknd</a></p>
-
-              </div>
-          </div> 
+						<div class="col-md-6 col-sm-12">
+							<p class="titleRow border-bottom"><span>Albums</span></p>
+							<p class="list-content">
+								1. <i>Miss Anthropocene</i> by <a href="search.html?artist=Grimes">Grimes</a><br>
+									2. <i>Heaven to a Tortured Mind</i> by <a href="search.html?artist=Yves Tumor">Yves Tumor</a><br>
+									3. <i>What's Your Pleasure?</i> by <a href="search.html?artist=Jessie Ware">Jessie Ware</a><br> 
+									4. <i>Shall We Go On Sinning So That Grace May Increase?</i> by <a href="search.html?artist=The Soft Pink Truth">The Soft Pink Truth</a><br>
+									5. <i>Punisher</i> by <a href="search.html?artist=Phoebe Bridgers">Phoebe Bridgers</a><br>
+									6. <i>Women in Music Pt. III</i> by <a href="search.html?artist=Haim">Haim</a><br>
+									7. <i>Fetch the Bolt Cutters</i> by <a href="search.html?artist=Fiona Apple">Fiona Apple</a><br>
+									8. <i>Inner Song</i> by <a href="search.html?artist=Kelly Lee Owens">Kelly Lee Owens</a><br>  
+									9. <i>Healing Is A Miracle</i> by <a href="search.html?artist=Julianna Barwick">Julianna Barwick</a><br>
+									10. <i>All The Time</i> by <a href="search.html?artist=Jessy Lanza">Jessy Lanza</a><br>  
+									Honorable EP Mention: <i>ALIAS</i> by <a href="search.html?artist=Shy Girl">Shy Girl</a><br> 
+							</p>	
+						</div>
+						
+						<div class="col-md-6 col-sm-12">
+							<p class="titleRow border-bottom"><span>Songs</span></p>
+							<p class="list-content">
+								1. "Sarita Colonia" by <a href="search.html?artist=Sofia Kourtesis">Sofia Kourtesis</a><br>
+								2. "Guilty Conscience" by <a href="search.html?artist=070 Shake">070 Shake</a><br>
+								3. "Gospel For A New Century" by <a href="search.html?artist=Yves Tumor">Yves Tumor</a><br>
+								4. "Baby" by <a href="search.html?artist=Four Tet">Four Tet</a><br>
+								5. "Remember Where You Are" by <a href="search.html?artist=Jessie Ware">Jessie Ware</a><br>
+								6. "XS" by <a href="search.html?artist=Rina Sawayama">Rina Sawayama</a><br>
+								7. "Home" by <a href="search.html?artist=Caribou">Caribou</a><br>
+								8. "For You" by <a href="search.html?artist=India Jordan">India Jordan</a><br>
+								9. "circle the drain" by <a href="search.html?artist=Soccer Mommy">Soccer Mommy</a><br>
+								10. "Blinding Lights" by <a href="search.html?artist=The Weeknd">The Weeknd</a>
+							</p>	
+						</div>
+						
+          </div>
 
           <hr>
 
@@ -269,36 +228,23 @@
           </div>
           
           <div class="row justify-content-center">
-                <div class="column text-center">
-                  <p class="titleRow"><span class="border-bottom">Albums</span></p>
-                </div>
-                <div class="column text-center">
-                  <p class="titleRow"><span class="border-bottom">Songs</span></p>
-                </div>
+            <div class="col-md-6 col-sm-12">
+							<p class="titleRow border-bottom"><span>Songs</span></p>
+							<p class="list-content">
+								1. "Protect the Land" by <a href="search.html?artist=System of a Down">System of a Down</a><br>
+								2. "New Colossal Hate" by <a href="search.html?artist=Lamb of God">Lamb of God</a><br>
+								3. "Everythings Right" by <a href="search.html?artist=Phish">Phish</a><br>
+								4. "Jessie's Girl 2" by <a href="search.html?artist=Coheed and Cambria">Coheed and Cambria</a><br>
+								5. "Apocalyptical" by <a href="search.html?artist=Puscifer">Puscifer</a><br>
+								6. "Fallen Torches" by <a href="search.html?artist=Mastodon">Mastodon</a><br>
+								7. "Coming of Age" by <a href="search.html?artist=Blondes">Blondes</a><br>
+								8. "Ach So Gern" by <a href="search.html?artist=Lindemann">Lindemann</a><br>
+								9. "Patience" by <a href="search.html?artist=Chris Cornell">Chris Cornell</a><br>
+								10. "Chop Suey" by <a href="search.html?artist=Nekrogoblikon">Nekrogoblikon</a><br>
+							</p>	
+						</div>
+						
           </div>
-
-            <div class="row">
-              <div class="column">
-
-              </div>
-
-              <div class="column">
-            
-                 <p>1. "Protect the Land" by <a href="search.html?artist=System of a Down">System of a Down</a><br>
-                    2. "New Colossal Hate" by <a href="search.html?artist=Lamb of God">Lamb of God</a><br>
-                    3. "Everythings Right" by <a href="search.html?artist=Phish">Phish</a><br>
-                    4. "Jessie's Girl 2" by <a href="search.html?artist=Coheed and Cambria">Coheed and Cambria</a><br>
-                    5. "Apocalyptical" by <a href="search.html?artist=Puscifer">Puscifer</a><br>
-                    6. "Fallen Torches" by <a href="search.html?artist=Mastodon">Mastodon</a><br>
-                    7. "Coming of Age" by <a href="search.html?artist=Blondes">Blondes</a><br>
-                    8. "Ach So Gern" by <a href="search.html?artist=Lindemann">Lindemann</a><br>
-                    9. "Patience" by <a href="search.html?artist=Chris Cornell">Chris Cornell</a><br>
-                    10. "Chop Suey" by <a href="search.html?artist=Nekrogoblikon">Nekrogoblikon</a><br>
-
-                </p>
-
-              </div>
-           </div> 
 
           <hr>
 
@@ -310,36 +256,22 @@
           </div>
           
           <div class="row justify-content-center">
-                <div class="column text-center">
-                  <p class="titleRow"><span class="border-bottom">Albums</span></p>
-                </div>
-                <div class="column text-center">
-                  <p class="titleRow"><span class="border-bottom">Songs</span></p>
-                </div>
+            <div class="col-md-6 col-sm-12">
+							<p class="titleRow border-bottom"><span>Songs</span></p>
+							<p class="list-content">
+								1. "Maid of the Mist" by <a href="search.html?artist=Diet Cig">Diet Cig</a><br>
+								2. "Prom Queen" by <a href="search.html?artist=Beach Bunny">Beach Bunny</a><br>
+								3. "Nightcall" by <a href="search.html?artist=Kavinsky">Kavinsky</a><br>
+								4. "Yesterday" by <a href="search.html?artist=Yumi Zouma">Yumi Zouma</a><br>
+								5. "Memphis" by <a href="search.html?artist=Kitten">Kitten</a><br>
+								6. "Lust for Life" by <a href="search.html?artist=Girls">Girls</a><br>
+								7. "Everybody Wants to Rule the World" by <a href="search.html?artist=Tears for Fears">Tears for Fears</a><br>
+								8. "One Headlight" by <a href="search.html?artist=The Wallflowers">The Wallflowers</a><br>
+								9. "Off the Record" by <a href="search.html?artist=My Morning Jacket">My Morning Jacket</a><br>
+								10. "Stay" by <a href="search.html?artist=Nekrogoblikon">Pronoun</a><br>
+							</p>	
+						</div>
           </div>
-
-            <div class="row">
-              <div class="column">
-
-              </div>
-
-              <div class="column">
-            
-                 <p>1. "Maid of the Mist" by <a href="search.html?artist=Diet Cig">Diet Cig</a><br>
-                    2. "Prom Queen" by <a href="search.html?artist=Beach Bunny">Beach Bunny</a><br>
-                    3. "Nightcall" by <a href="search.html?artist=Kavinsky">Kavinsky</a><br>
-                    4. "Yesterday" by <a href="search.html?artist=Yumi Zouma">Yumi Zouma</a><br>
-                    5. "Memphis" by <a href="search.html?artist=Kitten">Kitten</a><br>
-                    6. "Lust for Life" by <a href="search.html?artist=Girls">Girls</a><br>
-                    7. "Everybody Wants to Rule the World" by <a href="search.html?artist=Tears for Fears">Tears for Fears</a><br>
-                    8. "One Headlight" by <a href="search.html?artist=The Wallflowers">The Wallflowers</a><br>
-                    9. "Off the Record" by <a href="search.html?artist=My Morning Jacket">My Morning Jacket</a><br>
-                    10. "Stay" by <a href="search.html?artist=Nekrogoblikon">Pronoun</a><br>
-
-                </p>
-
-              </div>
-           </div> 
 
           <hr>
 
@@ -350,37 +282,22 @@
           </div>
           
           <div class="row justify-content-center">
-                <div class="column text-center">
-                  <p class="titleRow"><span class="border-bottom">Albums</span></p>
-                </div>
-                <div class="column text-center">
-                  <p class="titleRow"><span class="border-bottom">Songs</span></p>
-                </div>
+            <div class="col-md-6 col-sm-12">
+							<p class="titleRow border-bottom"><span>Songs</span></p>
+							<p class="list-content">
+								1. <i>Punisher</i> by <a href="search.html?artist=Phoebe Bridgers">Phoebe Bridgers</a><br>
+								2. <i>Honeymoon</i> by <a href="search.html?artist=Beach Bunny">Beach Bunny</a><br>
+								3. <i>Drug Dealing is a Lost Art</i> by <a href="search.html?artist=RMR">RMR</a><br>
+								4. <i>In Sickness & in Flames</i> by <a href="search.html?artist=The Front Bottoms">The Front Bottoms</a><br>
+								5. <i>Visions of Bodies Being Burned</i> by <a href="search.html?artist=Clipping.">Clipping.</a><br>
+								6. <i>What's Your Pleasure?</i> by <a href="search.html?artist=Jessie Ware">Jessie Ware</a><br>
+								7. <i>Heaven to a Tortured Mind</i> by <a href="search.html?artist=Yves Tumor">Yves Tumor</a><br>
+								8. <i>Set My Heart on Fire Immediately</i> by <a href="search.html?artist=Perfume Genius">Perfume Genius</a><br>
+								9. <i>Color Theory</i> by <a href="search.html?artist=Soccer Mommy">Soccer Mommy</a><br>
+								10. <i>RTJ4</i> by <a href="search.html?artist=Run the Jewels">Run the Jewels</a><br>
+							</p>	
+						</div>
           </div>
-
-            <div class="row">
-              <div class="column">
-              
-                 <p>1. <i>Punisher</i> by <a href="search.html?artist=Phoebe Bridgers">Phoebe Bridgers</a><br>
-                    2. <i>Honeymoon</i> by <a href="search.html?artist=Beach Bunny">Beach Bunny</a><br>
-                    3. <i>Drug Dealing is a Lost Art</i> by <a href="search.html?artist=RMR">RMR</a><br>
-                    4. <i>In Sickness & in Flames</i> by <a href="search.html?artist=The Front Bottoms">The Front Bottoms</a><br>
-                    5. <i>Visions of Bodies Being Burned</i> by <a href="search.html?artist=Clipping.">Clipping.</a><br>
-                    6. <i>What's Your Pleasure?</i> by <a href="search.html?artist=Jessie Ware">Jessie Ware</a><br>
-                    7. <i>Heaven to a Tortured Mind</i> by <a href="search.html?artist=Yves Tumor">Yves Tumor</a><br>
-                    8. <i>Set My Heart on Fire Immediately</i> by <a href="search.html?artist=Perfume Genius">Perfume Genius</a><br>
-                    9. <i>Color Theory</i> by <a href="search.html?artist=Soccer Mommy">Soccer Mommy</a><br>
-                    10. <i>RTJ4</i> by <a href="search.html?artist=Run the Jewels">Run the Jewels</a><br>
-
-                </p>       
-
-              </div>
-
-              <div class="column">
-
-
-              </div>
-           </div> 
 
        </div>
       </div>
