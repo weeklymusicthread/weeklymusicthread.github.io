0adb4ff5c01fe2175b526bf64eea32c583096f1a
Sam <sam.paderewski@gmail.com>
2021-01-11 17:18:26 -0500

    new post

---
+++ 2021-01-11.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+  
+  <link rel="icon" type="image/x-icon" href="img/favicon.ico" />
+
+  <title>Weekly Music Thread | Celebrit-E</title>
+
+  <?php include('php/header.php'); ?>
+
+</head>
+
+<body>
+
+  <?php include('php/navbar.php'); ?>  
+
+  <!-- Page Header -->
+  <header class="masthead" style="background-image: url('img/vaporheader.jpg')">
+    <div class="overlay"></div>
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-11 col-md-11 mx-auto">
+          <div class="post-heading">
+            <h1>Celebrit-E</h1>
+            <h2 class="subheading"></h2>
+            <span class="meta">Posted by
+              <a href="#">DJ Bette Davis Eyes Wide Shut</a>
+              on January 11, 2021</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </header>
+
+  <!-- Post Content -->
+  <article>
+    <div class="container">
+      <div class="row">
+	  	  
+        <div class="col-lg-11 col-md-11 mx-auto" id="postSection">
+         
+          <blockquote class="blockquote">"Intensive care unit" - Avatar (2009)</blockquote>
+
+          <p>We at the WMT have been thinking a lot lately about the way that society seems to be fracturing vis-a-vis the entertainment media landscape. Whereas before, most people got their television programming through three or so major networks, movies from big studios, books from big publishers, and music from big labels, now all of these things are increasingly democratized. All of us are more and more empowered to find ever smaller niches that fit our tastes, and are granted direct access to the artists who cater to those tastes. The age of blockbusters, platinum records, and New York Times best sellers probably isn't over, but these phenomena feel increasingly like the exceptions rather than the rules.</p>
+
+          <p>So too does separation between stars and their fans. Celebrities of the past nonetheless maintained some separation between themselves and the public, but social media has eroded that safe space. With accelerating access to the inner lives and thoughts of our celebrities--and lower and lower thresholds for their "cancellation"--are we moving into an era in which celebrity status itself is becoming both obsolete from a distribution perspective and almost "objectively" undesirable from a personal one?</p>
+          
+          <p>Take, for instance, Grimes. Increasingly, Grimes is separating her offline life from her digital life, which she is partitioning into her online avatar, WarNymph. Aside from allowing her to, for example, continue promotional photoshoots even throughout the late stages of her recent pregnancy, the avatar allows Grimes to sidestep the difficulties which come with being a celebrity in the age of social media. "It's hard on the psyche to have so much negativity thrown your way on a daily basis via social media. I hear about how ugly or stupid or annoying I am every day." We're sure hubby Elon Musk can sympathize. (No doubt this has been a subject of pillow talk between the two?)</p>
+
+          <p>Celebrity avatars also allow for engagement in ways that weren't possible before. Even in the midst of a pandemic, with large gatherings prohibited nearly worldwide, Travis Scott and other musicians were able to perform digital proxy concerts within Fortnite. We don't wonder so much whether these things will grow in popularity--it seems certain that they will--but rather how far it will go.</p>
+          
+          <p>WarNymph, for example, resembles Grimes, and is obviously a product of hers. Travis Scott performed "as himself" in Fortnite. These digital avatars are simply stand-ins for a real person who we know exists, and whose identity is known to us. But when will we witness the rise of (initially de facto) synthetic celebrities? When will meatspace humans start choosing to remain anonymous, and gain popular followings merely through their digital avatars? And when AI develops enough, when will organically synthetic celebrities arise out of its algorithms? Will we eventually swap <i>A Star is Born</i> for <i>An Avatar is Born</i>?</p>
+
+          <p>And once that's true in the art world, why wouldn't it be true in gaming, commentary, even politics? Politicians and journalists probably receive more vitriol than any other sector--who wouldn't outsource that to an emotionless AI, given the opportunity? Once celebrity is automated, and when personal details are plastered across every social media platform imaginable, moreover, will we see a flight to anonymity? Will all of those millions of books, movies, and songs about moving to New York or LA to try and make it big give way to works of art about the opposite? Are we in for a total reversal of all of these things?</p>
+
+          <h2 class="section-heading">And now, onto the show...</h2>
+          <br>
+        </div>
+
+        <div class="col-lg-11 col-md-11 mx-auto"> <!-- From The Vault/We're Stil Listening/Up The Punx-->
+         
+          <hr>
+
+          <h2 class="section-heading" style="font-size:125%">From the Vault</h2>
+
+          <p>This week, from the vault, we bring you Kim Carnes' 1981 on "Bette Davis Eyes." Although Carnes didn't write the track, she perfected it, with that sort of breathy, sort of scratchy singing voice that sends jealous girlfriends to rolling their eyes. (FYI, Brandon Flowers does a fun cover which makes this track sound like a Killers original.)</p>
+
+          <h2 class="section-heading" style="font-size:125%">We're Still Listening To</h2>
+
+          <p>Charli XCX, Carly Rae Jepsen, Lana del Rey, The Antlers, Grimes</p>
+
+         </div>
+        
+		
+		
+		
+      </div>
+    </div>
+  </article>
+
+  <hr>
+
+  <?php include('php/footer.php'); ?>  
+
+  <!-- Bootstrap core JavaScript -->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  <!-- Custom scripts for this template -->
+  <script src="js/clean-blog.js?uid=<?php echo uniqid(); ?>"></script>
+  
+  <script src="js/parse_music_entries.js?uid=<?php echo uniqid(); ?>"></script>
+  
+  <script>
+	displayPosts('2021-01-11');
+  </script>
+
+</body>
+
+</html>
