6ab976c89e10dfdb71e70befa23ec4cc2ce30112
Sam <sam.paderewski@gmail.com>
2021-01-18 17:04:58 -0500

    new post

---
+++ 2021-01-18.php
@@ -0,0 +1,120 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+  
+  <link rel="icon" type="image/x-icon" href="img/favicon.ico" />
+
+  <title>Weekly Music Thread | January 18, 2021</title>
+
+  <?php include('php/header.php'); ?>
+
+</head>
+
+<body>
+
+  <?php include('php/navbar.php'); ?>  
+
+  <!-- Page Header -->
+  <header class="masthead" style="background-image: url('img/vaporheader.jpg')">
+    <div class="overlay"></div>
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-11 col-md-11 mx-auto">
+          <div class="post-heading">
+            <h1>Test Your Metal</h1>
+            <h2 class="subheading"></h2>
+            <span class="meta">Posted by
+              <a href="#">DJ Drum Styx</a>
+              on January 18, 2021</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </header>
+
+  <!-- Post Content -->
+  <article>
+    <div class="container">
+      <div class="row">
+	  	  
+        <div class="col-lg-11 col-md-11 mx-auto" id="postSection">
+         
+          <blockquote class="blockquote">"You've got to know when to hold 'em, know when to fold 'em, know when to walk away, and know when to run" - Brandon Flowers</blockquote>
+
+          <p>And now, for the latest installment in <i>The Adventures of Johnny Drumsticks!</i></p>
+
+          <p>Some monikers are unearned. (Chinese Checkers are famously, for instance, neither Chinese, nor checkers.) But not so with Johnny Drumsticks! As a young man, JD lived up to his name, keeping both time and promise as he percussed his way through bands and across stages.</p>
+
+          <p>As with many amateur artists, Johnny D. and his bands were eager to get their name(s) out there, and would play for anyone who would listen. This, of course, included many venues in which people wouldn't listen, such as empty shopping mall parking lots, Chuck E. Cheese's, and even the occasional adolescent bday party. It also meant that you said yes to any gig and venue, without thoroughly (or often even at all) vetting them first.</p>
+
+          <p>So it was for a show one band picked up several hours away. The organizers asked J. Drumsticks and his associates to drive out for a show, and promised them gas money. (This was as good a living as any of them were ever going to make playing music, though they didn't know it at the time.) The boys excitedly accepted it. I mean, gas money? That's a government salary.</p>
+
+          <p>When they got there, however, things turned sour. The booking was a bad one; the organizers had meant to book a different garage band by the same name. Who, you might ask, would know the difference? Well, probably everybody, as this skinny jean indie rock outfit had just showed up to a death metal concert.</p>
+
+          <p>Nevertheless, they persisted. The boys bravely took the stage, and played to nobody in particular. Nobody, that is, until the metalheads started to notice that what they were hearing wasn't just uninteresting to them--it was actually actively offensive. Thus began the boos. They were isolated at first, individual. But soon they grew into a chorus which was impossible to ignore. As the boys finished one track, they huddled up for another.</p>
+
+          <p>"Is it even worth going on? These guys hate us."</p>
+
+          <p>"We can't just stop in the middle of a set."</p>
+
+          <p>"What if we just played the Killers cover and got out of here?"</p>
+
+          <p>Now here was a great idea. The Killers cover--"All of These Things That I've Done"--was a showstopper. The boys had been playing it for a year or two, and being as recognizable as the song was, it always got a good reaction out of the crowd. The lead turned to the mic: "Who here likes the Killers???"</p>
+
+          <p>At this point, one particularly loathsome metalhead in the second or third row of people stepped forward. For some reason, everyone else got quiet.</p>
+
+          <p>"The Killers fucking suck," he said coldly. "And so do you."</p>
+
+          <p>"Thank you," said the lead into the microphone, "and goodnight!" And with that, the boys hustled their gear, to the sound of further booing, off of the stage and into the van, escaping narrowly with their lives, but not their pride.</p>
+
+          <h2 class="section-heading">And now, onto the show...</h2>
+          <br>
+        </div>
+
+        <div class="col-lg-11 col-md-11 mx-auto"> <!-- From The Vault/We're Stil Listening/Up The Punx-->
+         
+          <hr>
+
+          <h2 class="section-heading" style="font-size:125%">From the Vault</h2>
+
+          <p>This week, from the vault, we bring you Makthaverskan's "Asleep." Makthaverskan are another Swedish group (readers will recall last week's review of "Cleansing Foam" by young Swedes Parsimon), and "Asleep" is a track off their 2014 effort, <i>II</i>. This one's got serious replay potential. It feels like the Beach Bunny aesthetic and songwriting approach adopted a shoegaze ethos and "Dammit"-era Blink 182 production quality. And its central refrain, "it's not me you're dreaming of," gets repeated over and over until all you can do is nod along and think to yourself that, "yeah, it's NOT me you're dreaming of!"</p>
+
+          <h2 class="section-heading" style="font-size:125%">We're Still Listening To</h2>
+
+          <p>Ethel Cain's <i>Golden Age</i>, Yumi Zouma's <i>Truth or Consequences</i>, Grimes' <i>Art Angels</i>, Carly Rae Jepsen's <i>Emotion Side B</i>, and Sigrid's <i>Sucker Punch</i></p>
+
+         </div>
+        
+		
+		
+		
+      </div>
+    </div>
+  </article>
+
+  <hr>
+
+  <?php include('php/footer.php'); ?>  
+
+  <!-- Bootstrap core JavaScript -->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  <!-- Custom scripts for this template -->
+  <script src="js/clean-blog.js?uid=<?php echo uniqid(); ?>"></script>
+  
+  <script src="js/parse_music_entries.js?uid=<?php echo uniqid(); ?>"></script>
+  
+  <script>
+	displayPosts('2021-01-18');
+  </script>
+
+</body>
+
+</html>
