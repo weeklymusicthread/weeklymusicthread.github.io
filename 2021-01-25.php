86e1e7e26afe4088a651776bd55405cb99df3326
Sam <sam.paderewski@gmail.com>
2021-01-25 12:33:08 -0500

    new post

---
+++ 2021-01-25.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+  
+  <link rel="icon" type="image/x-icon" href="img/favicon.ico" />
+
+  <title>Weekly Music Thread | January 25, 2021</title>
+
+  <?php include('php/header.php'); ?>
+
+</head>
+
+<body>
+
+  <?php include('php/navbar.php'); ?>  
+
+  <!-- Page Header -->
+  <header class="masthead" style="background-image: url('img/vaporheader.jpg')">
+    <div class="overlay"></div>
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-11 col-md-11 mx-auto">
+          <div class="post-heading">
+            <h1>The Tunes They Are-A Changin’</h1>
+            <h2 class="subheading"></h2>
+            <span class="meta">Posted by
+              <a href="#">DJ CD-ROM Emmanuel</a>
+              on January 25, 2021</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </header>
+
+  <!-- Post Content -->
+  <article>
+    <div class="container">
+      <div class="row">
+	  	  
+        <div class="col-lg-11 col-md-11 mx-auto" id="postSection">
+         
+          <blockquote class="blockquote">"“where we came in? […] Isn’t this…” – P!nk Floyd Mayweather</blockquote>
+
+          <p>When we were young [via les Tueurs], there was a different experience associated with listening to music. We are of such an age that our discovery of music came through CDs and portable CD players (or the gendered and highly problematic “Walkman,” if you will.) Actually, that’s not entirely true—the first experience we had with music ownership came in the form of an 8-track tape of the soundtrack of <i>Star Wars</i>. Shortly thereafter, however, we lobbied our parents for and subsequently received a Walkman. And once we had the weapon in our possession, we loaded it with ammunition. Our first records? Savage Garden’s <i>Savage Garden</i>, the Goo Goo Dolls’ <i>Dizzy Up the Girl</i>, Smashmouth’s <i>Astro Lounge</i>, and Chumbawamba’s <i>Tubthumper</i>. On a half-hour commute to lower school each morning, and on the return trip home, we rotated dutifully through these mid-to-late ‘90s works of art. </p>
+
+          <p>Over time, we, along with music listeners everywhere, evolved. No longer were we limited to the increasingly cracked jewel cases we could carry along with us—we upgraded to the CD wallet. And as we progressed through school and youth and our CD collection grew, so did the number of overflowing CD wallets we jammed into our overflowing Jansport backpacks every morning. But with more money comes, as the man says, more problems, and here was one: no longer could we afford to complete a rotation through all of our top hits on the round trip to and from school. We had to discriminate. And with the sheer number of CDs we were carrying, it became necessary over breakfast to devise a plan each morning. (CD wallets being almost uniformly a monochromatic grey-black, it was easy to become confused amongst them, and precious minutes could be lost in trying to locate the CD for the next single to which you had decided to listen. Hence, the plan.)</p>
+
+          <p>And thus we maximized our music consumption. But that wasn’t the only thing that was maximized in that era, and we weren’t just consuming our music—we were showing it off. The numbers and sizes of CD wallets became visible, tangible proof of our commitment to music and our taste in it. So did the state of those wallets. Nice, pristine wallets weren’t handled often, and so we knew the kids who had them weren’t really “into” music. Ours, on the other hand, were decrepit, which was how you knew that what they held was not. In those days, you could <i>see</i> what (and how much) music other people were listening to. </p>
+
+          <p>And then a funny thing started to happen. Some kids—usually the ones with a small number of nice, well kept CD wallets—stopped using CD wallets altogether, and started showing up to school with Runes, Zunes, and iPods. As purists, we of course scoffed and looked down our noses. How could these diminutive little trinkets represent anything other than an aesthetically (not to mention <i>morally</i>) hideous passing fad? And who could even tell how much music you listened to? </p>
+
+          <p>Well, you know how this story goes. Eventually, we all switched over. We of course were the last one to abandon our bloated CD wallets, but abandon them we did. Portable mp3 players simply offered us access to more music, and at the end of the day, that was really the most important thing. And anyway, over time, things like Last.fm and Spotify arose to take care of the rest. Now your friends could still see what (and how much) music you were consuming.</p>
+
+          <p>But suddenly, we find ourselves back in the past. You see, we have something to tell you: we’re currently bringing you the WMT from overseas, and will be for some time. And as part of that transition, we neglected to make major changes to our phone plan. (Come on—we’re getting free streaming services with it.) So we use WiFi and have access to basic data, but speeds get throttled over a certain limit. Which means that we are…right back where we started.</p>
+
+          <p>At first, we listened to Spotify only on the WiFi network at home. (Good luck connecting at work here.) But then we realized something—we could select a small number of records, download them, and listen to them in Spotify’s offline mode all day long. And so it was that, all of a sudden, we found ourselves right back where we started: carefully selecting, over breakfast each morning, a small number of songs and arranging them just so, so that we’d have access to them all day long. Our friends can no longer see what we’re streaming, because we’re no longer streaming. We are moving both forwards and backwards in time. We are <i>Tenet</i>. (Which also means that, if you don’t like this post, it isn’t simply that you don’t like it…it’s that <i>you didn’t get it</i>.)</p>
+
+          <p>And the re-discovery of this method of music consumption, decades later, brings with it the rediscovery of other forgotten flavors of that life. When we make a bad choice, we feel it for the entire walk to work. There is a sense of commitment to each song and/or record we download, and a real feeling of regret whenever we realize that that commitment has been misplaced. We triage, too. “Okay, I’d rather listen to X than Y right now, but I’ve only got the two, and I <i>know</i> I’m going to want to listen to X later, so I’ll start with Y.” When we discover that there is something new we’d like to hear—via text from a friend or some other such avenue—we actually have to <i>wait</i> until we get to a <i>physical location</i> where we can access it. And lucky for us if there’s a friend nearby whose got the album on their phone, because then they can play it for us. </p>
+
+          <p>There you have it. Time is a flat circle, and this is the only story ever written. <p>
+
+          <h2 class="section-heading">And now, onto the show...</h2>
+          <br>
+        </div>
+
+        <div class="col-lg-11 col-md-11 mx-auto"> <!-- From The Vault/We're Stil Listening/Up The Punx-->
+         
+          <hr>
+
+          <h2 class="section-heading" style="font-size:125%">From the Vault</h2>
+
+          <p>This week, from the vault, we bring you The Killers’ “Shot at the Night.” In other hands, this track would probably feel unbearably corny, but all of Brandon Flowers’ music is effused with such a naïve sense of innocent grandeur that this one comes together. (We wonder how much of that has to do with his Las Vegas roots.) At any rate, throw it on if you haven’t already—we’re sure it’ll have you saying to yourself, “Yeah, <b>give</b> me a shot at the night!” </p>
+
+          <h2 class="section-heading" style="font-size:125%">We're Still Listening To</h2>
+
+          <p>Pale Waves’ “Television Romance,” Katie Dey’s “Darkness,” Brandon Flowers’ “Lonely Town,” Young Jesus’ “(un)knowing,” and RMR’s “RASCAL”</p>
+
+         </div>
+        
+		
+		
+		
+      </div>
+    </div>
+  </article>
+
+  <hr>
+
+  <?php include('php/footer.php'); ?>  
+
+  <!-- Bootstrap core JavaScript -->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  <!-- Custom scripts for this template -->
+  <script src="js/clean-blog.js?uid=<?php echo uniqid(); ?>"></script>
+  
+  <script src="js/parse_music_entries.js?uid=<?php echo uniqid(); ?>"></script>
+  
+  <script>
+	displayPosts('2021-01-18');
+  </script>
+
+</body>
+
+</html>
