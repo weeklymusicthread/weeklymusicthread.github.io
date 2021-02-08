f19777ed3c248788dffe38a03d2962fd49642fd6
Sam <sam.paderewski@gmail.com>
2021-02-08 17:18:08 -0500

    new post

---
+++ 2021-02-08.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+  
+  <link rel="icon" type="image/x-icon" href="img/favicon.ico" />
+
+  <title>Weekly Music Thread | February 8, 2021</title>
+
+  <?php include('php/header.php'); ?>
+
+</head>
+
+<body>
+
+  <?php include('php/navbar.php'); ?>  
+
+  <!-- Page Header -->
+  <header class="masthead" style="background-image: url('img/vaporheader.jpg')">
+    <div class="overlay"></div>
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-11 col-md-11 mx-auto">
+          <div class="post-heading">
+            <h1>As I Lay Drying</h1>
+            <h2 class="subheading"></h2>
+            <span class="meta">Posted by
+              <a href="#">DJ William FaulkN.E.R.D.</a>
+              on February 8, 2021</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </header>
+
+  <!-- Post Content -->
+  <article>
+    <div class="container">
+      <div class="row">
+	  	  
+        <div class="col-lg-11 col-md-11 mx-auto" id="postSection">
+         
+          <blockquote class="blockquote">“How often have I lain beneath rain from my strange roof, thinking of home?” – Emily</blockquote>
+
+          <p>We write to you, as we’ve implied to this point, estranged and expatriated. And it <i>is</i> strange and…patriate? Whatever. At any rate, things—things like the customs, things like the language—are different. Even the simplest tasks become almost impossible in another country. (This is true to such a degree that you can apparently sell Netflix on at least one season of it. Then again, is that saying much?) Like, for instance, dealing with an increasingly aggressive leak in the ceiling of the apartment you moved into only two weeks ago.</p>
+
+          <p>We need not explain that this creates problems: problems with neighbors, problems with landlords, and problems with furniture. Problems with insurance companies. Not least of all, though, it creates problems with <i>sleeping</i>. And for several reasons. We are stressed, for one, by the situation itself, and by the anticipation of whatever it is that comes next. We are anxious that we will wake up damp or soaked or even afloat on the upholstered raft of our sofa bed. And then, most of all, we are persistently annoyed by the <i>drip, drip, drip</i> of this particular, spontaneous form of Chinese water torture.</p>
+
+          <p>And so it is that we plug our ears against the noise, first with fingers and then with pillows and then, finally, with ear buds. And slowly, as we struggle to fall asleep, we proceed retrograde through the back catalogue of an audiophile life, until we’re rediscovering records we haven’t heard in decades, and with them, the memories we were busy forming when we first discovered them. (If Proust had his <i>madeleines</i> then we have, we suppose, some songs on Spotify?) We’re by turns affected and embarrassed to remember both. Bus rides. Records that don’t hold up. A girl who kissed us once and who we later stood up.</p>
+
+          <p>We won’t share them here, and anyway, they wouldn’t land. So we’ll end just by saying that, once in a while, you should delve into your own back catalogue. You might be surprised by what it brings back with it.</p>
+
+          <h2 class="section-heading">And now, onto the show...</h2>
+          <br>
+        </div>
+
+        <div class="col-lg-11 col-md-11 mx-auto"> <!-- From The Vault/We're Stil Listening/Up The Punx-->
+         
+          <hr>
+
+          <h2 class="section-heading" style="font-size:125%">From the Vault</h2>
+
+          <p>This week, from the vault, we bring you…a change of pace. Maybe it’s just homesickness on the part of this expatriated American, but in those few moments when we’ve broken from all of the other great music mentioned above and below on this week’s installment of the WMT to come up for air, we’ve been revisiting George Strait’s “Check Yes or No.” This is the country music equivalent of a Glock: it’s just well built. Period.</p>
+
+          <h2 class="section-heading" style="font-size:125%">We're Still Listening To</h2>
+
+          <p>Videoclub’s “Enfance 80” and Sincere Engineer’s <i>Rhombithian</i> (seriously, this record gets better with every listen. And at this point, we’ve got a lot of listens under our belt.)</p>
+
+          <h2 class="section-heading" style="font-size:125%">Up the Punx</h2>
+
+          <p>Okay, so this isn’t exactly a typical “Up the Punx” segment, but we were really, really tickled by <a href="https://www.youtube.com/watch?app=desktop&v=ueiH3Xnu_uU">this video</a> of Alex Melton covering Barenaked Ladies’ “One Week” in the style of Blink 182. It really is uncanny how real it sounds.</p>
+
+         </div>
+        
+		
+      </div>
+    </div>
+  </article>
+
+  <hr>
+
+  <?php include('php/footer.php'); ?>  
+
+  <!-- Bootstrap core JavaScript -->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  <!-- Custom scripts for this template -->
+  <script src="js/clean-blog.js?uid=<?php echo uniqid(); ?>"></script>
+  
+  <script src="js/parse_music_entries.js?uid=<?php echo uniqid(); ?>"></script>
+  
+  <script>
+	displayPosts('2021-02-08');
+  </script>
+
+</body>
+
+</html>
