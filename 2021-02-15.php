9c25804adbb557c9c2e1aa707a075b5106ab4e1f
Sam <sam.paderewski@gmail.com>
2021-02-15 18:01:29 -0500

    new post

---
+++ 2021-02-15.php
@@ -0,0 +1,108 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+  
+  <link rel="icon" type="image/x-icon" href="img/favicon.ico" />
+
+  <title>Weekly Music Thread | February 15, 2021</title>
+
+  <?php include('php/header.php'); ?>
+
+</head>
+
+<body>
+
+  <?php include('php/navbar.php'); ?>  
+
+  <!-- Page Header -->
+  <header class="masthead" style="background-image: url('img/vaporheader.jpg')">
+    <div class="overlay"></div>
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-11 col-md-11 mx-auto">
+          <div class="post-heading">
+            <h1>Randumb & Dumber</h1>
+            <h2 class="subheading"></h2>
+            <span class="meta">Posted by
+              <a href="#">DJ Nassim Nicholas Taleb Kweli</a>
+              on February 15, 2021</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </header>
+
+  <!-- Post Content -->
+  <article>
+    <div class="container">
+      <div class="row">
+	  	  
+        <div class="col-lg-11 col-md-11 mx-auto" id="postSection">
+         
+          <blockquote class="blockquote">“Fool me once, shame on you. But fool me by randomness, shame on Natalie Portman.” – Black Swan director Nassim Nicholas Taleb</blockquote>
+
+          <p>As our faithful readers will shortly discover, nearly the entirety of this week’s WMT is devoted to a new group out of Bristol called Black Country, New Road. More on the group and their music below, but what we’re concerned with presently is, above all else, their name. Is it an homage to American women’s clothing retailer White House Black Market? A cryptic clue to some undisclosed geographic location? A Jordan Peele reimagining of a Cormac Macarthy novel? </p>
+
+          <p>It is, in fact, none of those things. No, instead, it is the output of a random phrase generator. Black Country, New Road named themselves randomly. How about that?</p>
+
+          <p>It’s not a bad name, by any stretch. Plenty of bands have named themselves…non-randomly? intentionally?...and done far worse. Radiohead, for instance, started off as “On a Friday,” because they used to practice…on Fridays. In fact, they went from the worst band name of all time to the best band name of all time based on a suggestion by their record label. So, things could have been a lot worse. </p>
+
+          <p>Still, there’s something going on with band names lately. (We’ve written elsewhere about the disturbing trend towards either pan-capitalization on the one hand or a complete absence of capitalization on the other.) The idea now of a record label forcing a group to change its name feels almost as ridiculous now as the idea of a record label at all. And there are even some groups which seem to choose their names as a middle finger to the idea of SEO, mass appeal, and commercial viability at all. (oOoOO, anyone?)</p>
+
+          <p>Black Country, New Road don’t seem to fall into that camp. Besides, if anything, naming yourself in opposition to “the market” is, in the end, still just defining yourself in relation to the market (albeit, admittedly, negatively). Naming yourself randomly—now <i>that’s</i> freedom. Your name doesn’t define you in either direction. In fact, in that case, it really doesn’t define you at all, now does it? </p>
+
+          <p>Maybe this is just a passing trend. Maybe it isn’t even a trend, and we’re making too much out of it. Or maybe band names will just mean less and less over time. Will there come a time, perhaps, when all of the band names are taken? Or, at least, all of the good ones? It seems like a ridiculous question, but who hasn’t given this some thought with respect to license plates and phone numbers? Language, of course, evolves, and culture with it—artist names like Elvis Depressedly and Michael Cera Palin would have been unimaginable before certain, very specific points in time—but do they evolve as quickly as “<i>the scene</i>”??</p>
+
+          <p>Anyway, the real concern here isn’t band names, as we could always just turn those into random number strings like phone numbers and/or license plates. The real concern is the music itself. Will there come a time when every possible combination of sounds, lyrics, and time signatures has been tried out? Will there really be nothing new under the sun? Given enough time, will an infinite number of monkeys banging on an infinite number of MIDI synthesizers eventually compose the complete works of…everyone?</p>
+
+          <p>Or maybe that time has already come. After all, countless hits are based around the same exact chord progressions, and radio waves were never big enough for David Bowie, Queen, Vanilla Ice, Lynyrd Skynyrd, and Warren Zevon. Is there nothing new under the sun? Actually, if Black Country, New Road have anything to say about it, we think that there just might be.</p>
+
+          <h2 class="section-heading">And now, onto the show...</h2>
+          <br>
+        </div>
+
+        <div class="col-lg-11 col-md-11 mx-auto"> <!-- From The Vault/We're Stil Listening/Up The Punx-->
+         
+          <hr>
+
+          <h2 class="section-heading" style="font-size:125%">From the Vault</h2>
+
+          <p>We realize that, at this point, we’ve said this about <i>most</i> songs, and thereby have severely undermined our own credibility on this front, but TLC’s “No Scrubs” might actually be the best song ever written. </p>
+
+          <h2 class="section-heading" style="font-size:125%">We're Still Listening To</h2>
+
+          <p>Ethel Cain’s <i>Golden Age</i></p>
+
+         </div>
+        
+		
+      </div>
+    </div>
+  </article>
+
+  <hr>
+
+  <?php include('php/footer.php'); ?>  
+
+  <!-- Bootstrap core JavaScript -->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  <!-- Custom scripts for this template -->
+  <script src="js/clean-blog.js?uid=<?php echo uniqid(); ?>"></script>
+  
+  <script src="js/parse_music_entries.js?uid=<?php echo uniqid(); ?>"></script>
+  
+  <script>
+	displayPosts('2021-02-15');
+  </script>
+
+</body>
+
+</html>
