17496647d95e0286e2e74b722dbb33d4ff4809cc
Sam <sam.paderewski@gmail.com>
2021-02-22 18:21:50 -0500

    new post

---
+++ 2021-02-22.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+  
+  <link rel="icon" type="image/x-icon" href="img/favicon.ico" />
+
+  <title>Weekly Music Thread | February 22, 2021</title>
+
+  <?php include('php/header.php'); ?>
+
+</head>
+
+<body>
+
+  <?php include('php/navbar.php'); ?>  
+
+  <!-- Page Header -->
+  <header class="masthead" style="background-image: url('img/vaporheader.jpg')">
+    <div class="overlay"></div>
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-11 col-md-11 mx-auto">
+          <div class="post-heading">
+            <h1>Bon L’ivre</h1>
+            <h2 class="subheading"></h2>
+            <span class="meta">Posted by
+              <a href="#">DJ Librarry Styles</a>
+              on February 22, 2021</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </header>
+
+  <!-- Post Content -->
+  <article>
+    <div class="container">
+      <div class="row">
+	  	  
+        <div class="col-lg-11 col-md-11 mx-auto" id="postSection">
+         
+          <p>This week, our adventures abroad continue. This time, we have branched out from just our wireless-less walk to work or our flooded, Emily-style apartment, and have spent the weekend at several different Paris bookstores.</p>
+
+          <p>Now, we have always liked bookstores. We were excited to get some time away from work to go check some of them out. We knew that it was going to be enjoyable for us. But we really were not prepared for just how enjoyable it was.</p>
+
+          <p>Imagine this: you’ve spent your entire life buried in either a book or a bookstore—often both at the same time—to the point that you can’t remember what it was like the first time you entered one. There was a first time, of course, but it’s lost to time. When was the first time you went to a movie theater? When was the first time, for that matter, that you heard music? To paraphrase a famous film, “you don’t have an answer for that, do you?” Now imagine that you get the chance to enter one for the first time again.</p>
+
+          <p>It didn’t hit us right away. It wasn’t until we wandered into the <i>littérature américaine</i> section, in fact, that we started to feel something. Because there, before us, were all of our favorite books in a way in which we had never seen them before. <i>Gravity’s Rainbow</i> had magically become <i>arc-en-ciel de gravité</i>. <i>As I Lay Dying</i> was now <i>tandis que j’agonise</i>. (Amusingly, <i>Absalom, Absalom!</i> was now <i>Absalon, Absalon!</i>) And it wasn’t just that the titles were different. <i>Everything</i> about the books seemed different: the descriptions on the back, the fact that the titles read up rather than down the spine, every little detail, really. And really, how marvelous it was to read the first lines of our favorite novels again <i>for the first time</i>, and in another language which we sort of speak and sort of don’t. It really felt like discovery.</p>
+
+          <p>Even the English-language editions held something special. If you’re like us and you’ve spent the better part of your waking life wandering through bookstores new and used, then it turns out that you’ve built up pattern recognition that you probably didn’t even know that you had. Because books are almost always sorted alphabetically, you are used to seeing certain books in certain places.</p>
+
+          <p>And we mean <i>books</i> here. Not authors or titles, but the physical things themselves. Because you’re also probably used to seeing certain classic, unchanging spines. The deep blue of <i>The Great Gatsby</i>, the golden tarnish of all of the Faulkner Vintage editions, the falling rocket on the side of <i>Gravity’s Rainbow</i>, and the changing, cloudy sky down the side of <i>Infinite Jest</i>. Yes, you recognize these designs when you see them, and maybe even more so, you recognize the dimensions of the works. Some, like <i>Gatsby</i>, are slender; others dominate their modest neighbors. When traversing the shelves of your local bookstore, these shapes are your real guides.</p>
+
+          <p>But those bodies were made for your market. When you leave that market, and travel to another one, what you’ll find is the same exact books (to a certain extent) but shoved into different bodies. Different bodies for different markets. And it is both jarring and delightful. Ayn Rand, for instance, is almost unrecognizable without her massive dimensions and her iconic art deco. The Knausgaards are a little less ostentatious and, as a result of that, they feel less like a set that go together. At each moment, you look for the shape of a book you know, and when you fail to find it, you feel for another moment altogether lost. And then you see that the book is right in front of you, exactly where it should be, but that it’s wearing somebody else’s skin. And rediscovering your old favorites in those different skins becomes, like bumping into a grade school friend in a new city after decades of estrangement, a sort of charming thrill. On the one hand, some of the same features are there; on the other, just look at how much has changed.</p>
+
+          <p>It’s hard to articulate how much fun this was for us. But we’ll sum it up like this: we’ve always loved books and bookstores, and this past weekend was like falling in love with them at first sight, all over again.</p>
+
+          <h2 class="section-heading">And now, onto the show...</h2>
+          <br>
+        </div>
+
+        <div class="col-lg-11 col-md-11 mx-auto"> <!-- From The Vault/We're Stil Listening/Up The Punx-->
+         
+          <hr>
+
+          <h2 class="section-heading" style="font-size:125%">From the Vault</h2>
+
+          <p>This week, we open up the vault to find Foxing’s third studio album, <i>Nearer My God</i>. Released in 2018, this one predates the WMT, but surely would have been featured here had we been around at the time. It’s an interesting record when resurfaces for us every winter. Given the deep freezes on the home front of late, we think now’s probably the time to bust it out if you haven’t already.</p>
+
+          <h2 class="section-heading" style="font-size:125%">We're Still Listening To</h2>
+
+          <p>Sincere Engineer’s <i>Rhombithian</i>, Yumi Zouma’s <i>Truth or Consequences</i>, Ethel Cain’s <i>Golden Age</i></p>
+
+         </div>
+        
+		
+      </div>
+    </div>
+  </article>
+
+  <hr>
+
+  <?php include('php/footer.php'); ?>  
+
+  <!-- Bootstrap core JavaScript -->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  <!-- Custom scripts for this template -->
+  <script src="js/clean-blog.js?uid=<?php echo uniqid(); ?>"></script>
+  
+  <script src="js/parse_music_entries.js?uid=<?php echo uniqid(); ?>"></script>
+  
+  <script>
+	displayPosts('2021-02-22');
+  </script>
+
+</body>
+
+</html>
