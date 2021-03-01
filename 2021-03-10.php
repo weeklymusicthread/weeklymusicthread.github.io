bee4b30662dd1114d39a422dfebfc01a0b944df9
Sam <sam.paderewski@gmail.com>
2021-03-01 17:29:59 -0500

    new post

---
+++ 2021-03-10.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+  
+  <link rel="icon" type="image/x-icon" href="img/favicon.ico" />
+
+  <title>Weekly Music Thread | March 1, 2021</title>
+
+  <?php include('php/header.php'); ?>
+
+</head>
+
+<body>
+
+  <?php include('php/navbar.php'); ?>  
+
+  <!-- Page Header -->
+  <header class="masthead" style="background-image: url('img/vaporheader.jpg')">
+    <div class="overlay"></div>
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-11 col-md-11 mx-auto">
+          <div class="post-heading">
+            <h1>(Daft) Punk is Dead</h1>
+            <h2 class="subheading"></h2>
+            <span class="meta">Posted by
+              <a href="#">DJ Raft Sunk</a>
+              on March 1, 2021</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </header>
+
+  <!-- Post Content -->
+  <article>
+    <div class="container">
+      <div class="row">
+	  	  
+        <div class="col-lg-11 col-md-11 mx-auto" id="postSection">
+
+          <blockquote class="blockquote">“All good things must come to an end” – Guy-Manuel de Homem-Cristo and Thomas Bangalter</blockquote>
+         
+          <p>One of the drawbacks to running a weekly music thread is that, six times out of seven, we’re behind the times. And so it is this week, with the demise of Daft Punk. Surely, by now, anyone who would read a blog like this is already well aware of the break-up. But nevertheless, we can’t help reminiscing.</p>
+
+          <p>We remember, for instance, listening to “Digital Love” on repeat for days on end with roommates in our college dorms, as both a prelude to pregames and a soundtrack to silly college crushes. (By the way, does anyone else hear the line “There’s nothing wrong/with just a little, little fun” as “There’s nothing wrong/with just a little elephant”?) Or the way that <i>Random Access Memories</i> took over parties and hangouts for a solid six months when it came out in 2013. Or, maybe most of all, our belated discovery just last year of the surprisingly fun <i>Tron: Legacy</i>, in which Daft Punk make a cameo.</p>
+
+          <p>On that last note, the complete edition of the original soundtrack for <i>Tron: Legacy</i> is really excellent. It’s up there for us with the soundtracks for <i>The Lord of the Rings</i> and Jim Jarmusch’s <i>Dead Man</i> (in which Johnny Depp and Gary Farmer read the poetry of William Blake over Neil Young’s electric guitar) as some of the best writing music there is. (By the way, if you haven’t seen <i>Dead Man</i> a hundred times by now, or listened to the original soundtrack a thousand, it’s kind of like, what are you waiting for, you know?)</p>
+
+          <h2 class="section-heading">And now, onto the show...</h2>
+          <br>
+        </div>
+
+        <div class="col-lg-11 col-md-11 mx-auto"> <!-- From The Vault/We're Stil Listening/Up The Punx-->
+         
+          <hr>
+
+          <h2 class="section-heading" style="font-size:125%">From the Vault</h2>
+
+          <p>This week, from the vault, we bring you Kanye West’s ye. Actually, the timing of this feels fitting, because Kanye is, in a way, a lot like Daft Punk (as well as Radiohead), in that, over the course of a long career, he has consistently both reinvented himself and continued to make music that is absolutely fire. Neither of those things is easy to do alone; to do them <i>at the same time</i> basically guarantees legend status. So, this week, revisit <i>ye</i>.</p>
+
+          <h2 class="section-heading" style="font-size:125%">We're Still Listening To</h2>
+
+          <p>Yumi Zouma’s <i>Truth or Consequences</i></p>
+
+         </div>
+        
+		
+      </div>
+    </div>
+  </article>
+
+  <hr>
+
+  <?php include('php/footer.php'); ?>  
+
+  <!-- Bootstrap core JavaScript -->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  <!-- Custom scripts for this template -->
+  <script src="js/clean-blog.js?uid=<?php echo uniqid(); ?>"></script>
+  
+  <script src="js/parse_music_entries.js?uid=<?php echo uniqid(); ?>"></script>
+  
+  <script>
+	displayPosts('2021-03-01');
+  </script>
+
+</body>
+
+</html>
