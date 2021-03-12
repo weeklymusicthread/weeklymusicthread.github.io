6659f3452f0f2b284744070a720f623bb93a657b
Sam <sam.paderewski@gmail.com>
2021-03-11 20:16:13 -0500

    new site design

---
+++ php/header2.php
@@ -0,0 +1,30 @@
+  <!-- Global site tag (gtag.js) - Google Analytics -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=G-FE1SBK50BE"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-FE1SBK50BE');
+  </script>  
+  
+  <!-- Bootstrap core CSS -->
+  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Custom fonts for this template -->
+  <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+  <link href='https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
+  <link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
+
+  <!-- Custom styles for this template -->
+  <link href="css/clean-blog2.css?<?php echo time(); ?>" rel="stylesheet">
+	<link href="css/search-bar.css?<?php echo time(); ?>" rel="stylesheet">
+
+ <!-- Twitter Card tags -->
+  <meta name="twitter:card" content="summary" />
+  <meta name="twitter:site" content="@weeklymusicsite" />
+  <meta name="twitter:title" content="Weekly Music Thread" />
+  <meta name="twitter:description" content="Read the full article on Weekly Music Thread" />
+  <meta name="twitter:image" content="https://weeklymusicthread.com/img/twitcardimage.jpg"/>
+
+  
