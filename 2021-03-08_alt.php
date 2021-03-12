5ffac532f73e95653f6d6440d96acac12c43c850
Sam <sam.paderewski@gmail.com>
2021-03-12 12:38:07 -0500

    add sample new format article

---
+++ 2021-03-08_alt.php
@@ -0,0 +1,119 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+  
+  <link rel="icon" type="image/x-icon" href="img/favicon.ico" />
+
+  <title>Weekly Music Thread | March 8, 2021</title>
+
+  <?php include('php/header.php'); ?>
+
+</head>
+
+<body>
+
+  <?php include('php/navbar.php'); ?>  
+
+  <!-- Page Header -->
+  <header>
+  <div class="container">
+      <div class="row">
+        <div class="col-lg-10 col-md-10 mx-auto">
+          <br>
+          <br>
+          <br>
+          <div style="font-size:12px; color:rgb(142, 47, 231);"> <b>WEEKLY ARTICLE </b></div>
+          <br>
+          <div class="post-heading">
+            <h1>Quantum of Playlist</h1>
+            <h2 class="subheading"></h2>
+            <span class="meta" style="font-size:14px;">By
+              <a href="#">DJ Monte Carlos Santana</a> /
+              March 8, 2021</span>
+          </div>
+
+          <br>
+          <img src="img/i_quantumofplaylist.jpg" alt="Card image cap" style="width:90%;height:495px;">
+          <br>
+          <br>
+          <br>
+
+        </div>
+      </div>
+    </div>
+  </header>
+
+  <!-- Post Content -->
+  <article>
+    <div class="container">
+      <div class="row">
+	  	  
+        <div class="col-lg-10 col-md-10 mx-auto" id="postSection">
+
+          <blockquote class="blockquote">“The arc of the aural universe is long but it bends toward music” -  If Spotify user profiles had “About Me” sections</blockquote>
+         
+          <p>Imagine that you are eating at a restaurant. (Are there any Californians in the audience tonight?) Imagine that you try to place an order at this restaurant, but you’re told that you cannot. The options, it turns out, are fixed, and if you want to eat, you’ll have to eat what the restaurant decides to serve.</p>
+
+          <p>Are you in a nice restaurant or a bad one?</p>
+
+          <p>It could go either way. Because this is what some of the worst and some of the nicest restaurants in the entire world have in common: diners are limited in their choices. Now, in the case of all-you-can-eat hog troughs like Cici’s Pizza, for instance, this is because the dining options are determined not by consumer choice, but by algorithms designed to maximize profit and minimize food waste by streamlining every decision from what to serve to when and where to serve it.</p>
+
+          <p>In the case of slightly higher end restaurants, the reasoning is a little different. Here, what we have is not an arms race of nickel-and-diming toward an obesity epidemic, but rather, the offer of a totally curated experience. In other words, you do not need to evaluate the options and choose what you would like to order, because the chef, who knows better than you do, has already done that for you. The chef has designed the menu and the experience. You’re just here to taste it.</p>
+
+          <p>This is a lot like listening to a record, and it’s why it’s important that, when a record is written and recorded as a record—which is to say, as a greater piece than just the sum of its songs alone—it’s important to listen to it <i>in order</i>. The listening experience has already been designed. It’s been curated. Like ginger in between bites of raw fish, or an amuse-bouche before and/or between starters, track listings are put in place to make sure that the transition from one song to the next is as purposeful as possible. This is <i>also</i> why it’s so frustrating when you finish the opener of an album only to have your expectations as to the next track foiled when you realize that you accidentally left Spotify set to shuffle.</p>
+
+          <p>This is, without a doubt, the worst thing that ever happens to anybody. You are expecting a cocktail and you wind up with a dessert wine; you reach for an after-dinner mint and it tastes like foie gras. The intrusion of the shuffle setting into records is, in a similar way, a daily frustration for us.</p>
+
+          <p>Things are, of course, the exact opposite for playlists. Although some playlists are designed with a specific order in mind, a whole bunch of them—Spotify’s personalized Top Songs of 2020, for instance—are not. A playlist is governed not by order, artistry, and hierarchy, but rather by like-mindedness. Here are a whole bunch of songs you liked last year. Here are a whole bunch of songs that are great for a pregame. Here are a whole bunch of songs that ____________. Actually, playlists are probably the reason we have a shuffle option to begin with: because listening to them in order would flatten them from collections into queues.</p>
+
+          <p>So here is our modest proposal. We, the undersigned, <i>demand</i> that Spotify both a) REMOVE the shuffle option from albums and b) make it MANDATORY for playlists. The world is watching.</p>
+
+          <h2 class="section-heading">And now, onto the show...</h2>
+          <br>
+        </div>
+
+        <div class="col-lg-11 col-md-11 mx-auto"> <!-- From The Vault/We're Stil Listening/Up The Punx-->
+         
+          <hr>
+
+          <h2 class="section-heading" style="font-size:125%">From the Vault</h2>
+
+          <p>This week, from the vault, we bring you Bob Dylan’s enigmatic “Mr. Tambourine Man.” Pair it with Billy Joel’s “Piano Man,” Nicki Minaj’s “Super Bass,” and “The Little Drummer Boy,” and you’ve either got yourself a full ensemble or the worst playlist of all time.</p>
+
+          <h2 class="section-heading" style="font-size:125%">We're Still Listening To</h2>
+
+          <p>The Tallest Man on Earth’s <i>Dark Bird is Home</i></p>
+
+         </div>
+        
+		
+      </div>
+    </div>
+  </article>
+
+  <hr>
+
+  <?php include('php/footer.php'); ?>  
+
+  <!-- Bootstrap core JavaScript -->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  <!-- Custom scripts for this template -->
+  <script src="js/clean-blog.js?uid=<?php echo uniqid(); ?>"></script>
+  
+  <script src="js/parse_music_entries.js?uid=<?php echo uniqid(); ?>"></script>
+  
+  <script>
+	displayPosts('2021-03-08');
+  </script>
+
+</body>
+
+</html>
