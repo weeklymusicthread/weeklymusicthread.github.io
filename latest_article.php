1e01811b800769698045f3774c1c6651539acb58
Sam <sam.paderewski@gmail.com>
2021-03-18 17:39:45 -0400

    new loading gif

--- latest_article.php
+++ latest_article.php
@@ -12,6 +12,18 @@
 
   <title>Weekly Music Thread | Latest Article</title>
 
+  <style>
+  .img-center {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    width: 300px;
+    height: 300px;
+    margin-top: -150px; /* Half the height */
+    margin-left: -150px; /* Half the width */
+  }
+  </style>
+
   <?php include('php/header.php'); ?>
 
 
@@ -19,7 +31,8 @@
 
 <body id="redirectHeader">
 
- Redirecting...
+  <script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
+  <lottie-player src="https://weeklymusicthread.com/lf30_editor_wpqihs6i.json" class="img-center" background="transparent"  speed="1" loop  autoplay></lottie-player>
 
   <script src="vendor/jquery/jquery.min.js"></script>
   <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
