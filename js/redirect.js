710f45c6bf0e592576625a8d42b102d0609063a1
Sam <sam.paderewski@gmail.com>
2021-03-18 17:42:45 -0400

    new loading gif

--- js/redirect.js
+++ js/redirect.js
@@ -7,7 +7,7 @@ function redirect() {
 	// jquery function to get the json file and then put it into memory as "postData" var
 	$.getJSON('https://weeklymusicthread.com/post_previews.json', postData => {
 
-		htmlString += `<meta http-equiv="refresh" content="0; URL=https://www.weeklymusicthread.com/`;
+		htmlString += `<meta http-equiv="refresh" content="1; URL=https://www.weeklymusicthread.com/`;
 		htmlString += postData[0].post_page;
 		htmlString += `.php`;
 		htmlString += `" />`
