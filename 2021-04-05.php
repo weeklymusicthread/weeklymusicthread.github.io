c526b32ecbb6c51c5f088de61c51a201cbf8dcea
Sam <sam.paderewski@gmail.com>
2021-04-05 12:05:08 -0500

    new post

---
+++ 2021-04-05.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head id="pageHead">
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+  
+  <link rel="icon" type="image/x-icon" href="img/favicon.ico" />
+
+  <title>Weekly Music Thread | April 5, 2021</title>
+
+  <?php 
+        include('php/header.php'); 
+        genTwitterCard('2021-04-05');
+  ?>
+
+</head>
+
+<body>
+
+  <?php include('php/navbar.php'); ?>  
+
+  <!-- Page Header -->
+  <header id="articleHeader">
+
+  </header>
+
+  <!-- Post Content -->
+  <article>
+    <div class="container">
+      <div class="row">
+	  	  
+        <div class="col-lg-10 col-md-10 mx-auto" id="postSection">
+
+          <p>When we were young [via the murderers], music hadn't yet become a cloud-based app which you could jailbreak out of your phone like it is today. At that time, music came in physical forms: vinyl, tapes, CDs. (When we were young [via the executioners], we were apparently young for a period spanning several decades.) And because music was confined to meatspace in the totem forms of these physical hardwares, it was something that you could <i>own</i>.</p>
+
+          <p>Well, okay. Maybe you didn’t literally own the music itself. That was for the suits at the labels. But when you bought a record, a tape, or a CD, you owned the only way that you could listen to the music, and that at least was the next best thing. </p>
+
+          <p>This made a certain amount of sense to everyone. After all, if you pay for a CD, it seems fair that you would get first say over anything that ends up happening to that CD—that you get to settle any conflicts, should they arise. And because CDs were scarce and exclusive resources—because there were a finite number of them, any one of which could only be controlled by one specific individual at a time—conflicts could indeed arise. </p>
+
+          <p>(These, of course, were not the only sorts of conflicts which arose over CDs. There were also conflicts of the sort which arose when, after middle school basketball practice one afternoon, we caught a ride home with a friend and his father. Things were going fine until our friend, who was riding shotgun, wordlessly began to play his new Sean Paul CD. Conflict arose after a minute or two when his father merged onto the highway, wordlessly ejected it, and threw it out of the window of his 2005 Honda Pilot.)</p>
+
+          <p>For these reasons, back then, you could also <i>steal</i> music. Anything which can be owned—the notable exceptions, of course, being both “noobs” and impromptu, “You Got Served”-style street dance contest participants—can also be stolen. And so you could steal someone’s CD and lord control over it yourself. Or throw it out of a car window. Whatever. </p>
+
+          <p>All of this, however, changed with the MP3. Because MP3s are digital audio files, they are fungible and infinitely reproducible. Any given MP3 is just like any other given MP3, in other words, and we can copy them as many times as we want. As a result, they do not feature the same exclusivity as prior forms of audio recording. In no recognizable terms can you own them, steal them, or throw them out of the windows of family SUVs. </p>
+
+          <p>This was lost on at least four people. In addition to the members of Metallica, however, they were likely many others who were confused by the transition of consumers away from ownable, stealable, Honda Pilot-jettisonable recording formats and toward these more clonable MP3s. We were offered visions of a world in which the members of Metallica went hungry while nefarious college students compiled armamentaria of Napster-, Kazaa-, and Limewire-facilitated piracy! To illegally download a song, we were told, was no different from walking out of FYE/Best Buy/Sam Goody with a CD hidden inside of your jacket.</p>
+
+          <p>Only, it was quite obviously different, which is why so many more people illegally downloaded music than shoplifted from now defunct CD stores. When you steal a CD, you steal something that belongs to someone else, and you decrease the available units of that thing by one. When you rip an MP3, you exclude no one from anything—you are making a copy of something to which the original user still has access. That doesn’t necessarily make it right or wrong, but it makes it quite obviously, fundamentally different. And the fact that Metallica couldn’t see that means that we maybe—<i>maybe</i>—should reconsider their place as the mainstream heavy metal moral arbiters of our society. The arc of the moral universe is long, after all, and it bends like…metal? Or maybe it’s mixed…like metaphors.</p>
+
+          <p>Anyway, none of what Metallica predicted came to pass. Not even 20 years after the doomsday predictions, consumers are <i>willfully</i> switching away from illegally downloading music and opting instead for streaming services like Spotify and Tidal. (Well, okay, not Tidal.) </p>
+
+          <p>Enter non-fungible tokens, or "NFTs" (pronounced "JIF"). Created using blockchain technology (which we, like everyone else, merely <i>pretend</i> to understand and therefore will avoid mentioning from this point on), NFTs are unique, non-interchangeable files. We won't go into this further here--you can read more about it on literally every single site on the internet--but basically it's like if artists autographed their mp3 files. </p>
+
+          <p>For that reason, NFTs reintroduce scarcity and exclusivity to the consumption of music. Unlike MP3s and streams, NFTs can be owned, which means they can also be stolen. We’re not really sure what to make of them yet. Are they just a fad? Are they the recording format of the future? Are they best reserved for selling Jack Dorsey’s tweets? Are they easy to throw out of the window of family-friendly midsize crossover SUVs moving at top speeds down a North Carolina interstate? Can somebody get Metallica on the phone? How long <i>is</i> the arc of the moral universe? Is there an amoral universe? What about an immoral one? How long are <i>their</i> arcs? What do <i>they</i> bend toward? Can somebody get Theodore Parker on the phone?</p>
+
+          <h2 class="section-heading">And now, onto the show...</h2>
+          <br>
+        </div>
+
+        <div class="col-lg-10 col-md-10 mx-auto"> <!-- From The Vault/We're Stil Listening/Up The Punx-->
+         
+          <hr>
+
+          <h2 class="section-heading" style="font-size:125%">From the Vault</h2>
+
+          <p>This week, from the vault, we bring you The National's "Bloodbuzz Ohio." We used to really roll to this one back when <i>High Violet</i> came in. You could even say that it was in our honey/it was in our milk [via incognito parental administration of oral contraceptives and/or antidepressants to unknowing adolescents]. Actually, now that we explain that line, the reference doesn’t really make sense anymore. Our parents weren’t surreptitiously slipping us <i>High Violet</i>. To tell you the truth, they don’t even listen to The National! At any rate, the lyrics to this one are opaque yet meaningful. (Or, wait…perhaps they are opaque and <i>therefore</i> meaningful…)</p>
+
+          <h2 class="section-heading" style="font-size:125%">We're Still Listening To</h2>
+
+          <p>Ethel Cain's "Crush" (obviously), Sincere Engineer’s <i>Rhombithian</i> (again, obviously), Broke Royals'/Bartees Strange's "Goldbrick Champion," Katie Dey's "Darkness," Sorority Noise's "Rory Shield," Heart Attack Man's "Fake Blood," and Pale Waves' "Television Romance" (also, frankly, obvious)</p>
+
+         </div>
+        
+		
+      </div>
+    </div>
+  </article>
+
+  <hr>
+
+  <?php include('php/footer.php'); ?>  
+
+  <!-- Bootstrap core JavaScript -->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  <!-- Custom scripts for this template -->
+  <script src="js/clean-blog.js?uid=<?php echo uniqid(); ?>"></script>
+  
+  <script src="js/parse_music_entries.js?uid=<?php echo uniqid(); ?>"></script>
+
+  <script src="js/auto_article_header.js?uid=<?php echo uniqid(); ?>"></script>
+  
+  <script>
+	displayPosts('2021-04-05');
+  displayArticleHeader('2021-04-05');
+  </script>
+
+</body>
+
+</html>
