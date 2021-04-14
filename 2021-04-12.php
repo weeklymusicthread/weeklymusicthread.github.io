45153bb5c2ada2fbeb5b0d188b96895b0f5c97a1
Sam <sam.paderewski@gmail.com>
2021-04-14 11:15:12 -0500

    new post

---
+++ 2021-04-12.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head id="pageHead">
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+  
+  <link rel="icon" type="image/x-icon" href="img/favicon.ico" />
+
+  <title>Weekly Music Thread | April 12, 2021</title>
+
+  <?php 
+        include('php/header.php'); 
+        genTwitterCard('2021-04-12');
+  ?>
+
+</head>
+
+<body>
+
+  <?php include('php/navbar.php'); ?>  
+
+  <!-- Page Header -->
+  <header id="articleHeader">
+
+  </header>
+
+  <!-- Post Content -->
+  <article>
+    <div class="container">
+      <div class="row">
+	  	  
+        <div class="col-lg-10 col-md-10 mx-auto" id="postSection">
+
+          <blockquote class="blockquote">“I’m not like a *regular* president. I’m a *cool* president.” – Barack Obama</blockquote>
+
+          <p>It’s by now old news to many that Bruce Springsteen and Barack Obama have created a podcast together. The fact of the podcast’s existence isn’t what struck us—although, more on that below—so much as what they decided to call it: “Renegades: Born in the USA.” (Yes, you read that correctly. That’s actually the schizophrenic title of a single podcast.)</p>
+
+          <p>If you listen hard enough, you can almost hear the brainstorming session what birthed this two-headed, half-aborted monstrosity of a title: Springsteen insisting that his appearance as one of the country’s most popular recent musical artists alongside one of the country’s most popular recent presidents on the world’s most popular streaming platform market itself with some combination of “rebel” and “renegade” (and, very probably, “America,” “Chevrolet,” “road,” “street”—Jesus, maybe Biden should have considered him for DOT—“Mary,” et cetera) with Obama repeatedly insisting on incorporating into the title “Born in the USA” as both a reference to the only Springsteen song he probably knows and a bizarre dig at the dead-and-buried birtherism conspiracy theories that bubbled up during his presidency. Sure. Why not?</p>
+
+          <p>Eventually, the two decided to split the baby and just go with “Renegades: Born in the USA.” Aside from the fact that, again, <i>neither</i> of these men could be considered anything remotely close to a renegade—Springsteen has been signed to one of the world’s oldest and largest record labels for the past 50 (!) years, and Obama remains the most popular member of the most powerful political party in probably the history of the world—the grammatical construction of this title doesn’t make any sense at all. Why is there a colon between “Renegades” and “Born in the USA”? Colons are typically used as a signal that the information which follows them will somehow modify the information which immediately preceded them. How does “Born in the USA” in any way modify “Renegades”? Is that really a clarification that needed to be made? Is birth place important to one’s status as a renegade? Is this some sort of Macbeth/no man of woman born/Great Birnam Wood to high Dunsinane Hill situation? Or is this just what happens when you try to sum up in a title the point of a podcast which, in fact, has no point? (By the way, is there anything that screams “Renegade” less than two old guys launching a podcast now in the year 2021?)</p>
+
+          <p>And about that. “The Bruce Springsteen and Barack Obama Podcast,” P4k assures us, “Is Just Two Guys Talking About Hope.” (Not even <i>P4k</i> can bring themselves to use the name—they go on to suggest instead that it should have been called <i>Hope Guys</i>, which is, somehow, even worse.) This is a little like saying that Michael Jordan’s appearance in <i>Space Jam</i> was just a guy playing basketball. The entire point was <i>who</i> that guy was. Plenty of people play basketball every day and nobody stops to watch them. And so nobody would have seen (much less <i>made</i>) <i>Space Jam</i> if the guy in question hadn’t been who he was, and the same is true for this synthetic corporate facsimile of authentic self-expression. It’s the exact opposite of “just two guys.” It’s <i>these</i> two guys, and that’s the whole thing here. </p>
+
+          <p>So let’s stop pretending. If you really wanted to see “just two guys” talking about “some banal topic,” why not actually make that a podcast? You just pick two totally random people, and have them discuss with one another some completely random topic. And, honestly, even if you just stuck entirely to Americans—given the sheer size and heterogeneity of this country—you’d actually probably wind up with some pretty interesting conversations. Shit, you could call <i>that</i> “Born in the USA,” and then, that part of the title would actually, you know, <i>work</i>. <i>Make sense</i>. Feign a shallow meaning before offering up, post-consumption, something that then feels more meaningful. You know, the sort of thing that titles are supposed to do?</p>
+
+          <p>But we guess you could also just call it “Renegades: Born in the USA.”</p>
+
+          <h2 class="section-heading">And now, onto the show...</h2>
+          <br>
+        </div>
+
+        <div class="col-lg-10 col-md-10 mx-auto"> <!-- From The Vault/We're Stil Listening/Up The Punx-->
+         
+          <hr>
+
+          <h2 class="section-heading" style="font-size:125%">From the Vault</h2>
+
+          <p>This week, from the vault, we bring you, once again, alt-j’s debut album, <i>An Awesome Wave</i>. This is as close to a perfect debut as they come: every song is perfect, and they flow into each other seamlessly. Pitchfork even gave it a 4.8, which is how you know it’s good. (They cited the fact that it was a top 20 album in the UK as evidence for this low score.) So go ahead—start your morning off with a little alternative jones.</p>
+
+          <h2 class="section-heading" style="font-size:125%">We're Still Listening To</h2>
+
+          <p>Broke Royals and Bartees Strange’s “Goldbrick Champion,” Ethel Cain’s “Crush,” and Sincere Engineer’s <i>Rhombithian</i> (like, constantly)</p>
+
+         </div>
+        
+		
+      </div>
+    </div>
+  </article>
+
+  <hr>
+
+  <?php include('php/footer.php'); ?>  
+
+  <!-- Bootstrap core JavaScript -->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  <!-- Custom scripts for this template -->
+  <script src="js/clean-blog.js?uid=<?php echo uniqid(); ?>"></script>
+  
+  <script src="js/parse_music_entries.js?uid=<?php echo uniqid(); ?>"></script>
+
+  <script src="js/auto_article_header.js?uid=<?php echo uniqid(); ?>"></script>
+  
+  <script>
+	displayPosts('2021-04-12');
+  displayArticleHeader('2021-04-12');
+  </script>
+
+</body>
+
+</html>
