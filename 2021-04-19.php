baba71b60c068c301f56385b816db095015e90d5
Sam <sam.paderewski@gmail.com>
2021-04-27 22:27:26 -0400

    new posts

--- 2021-04-19.php
+++ 2021-04-19.php
@@ -10,11 +10,11 @@
   
   <link rel="icon" type="image/x-icon" href="img/favicon.ico" />
 
-  <title>Weekly Music Thread | April 19, 2021</title>
+  <title>Weekly Music Thread | April 26, 2021</title>
 
   <?php 
         include('php/header.php'); 
-        genTwitterCard('2021-04-19');
+        genTwitterCard('2021-04-26');
   ?>
 
 </head>
@@ -35,19 +35,25 @@
 	  	  
         <div class="col-lg-10 col-md-10 mx-auto" id="postSection">
 
-          <blockquote class="blockquote">And now, for the latest in…The Adventures of Johnny Science!</blockquote>
+          <blockquote class="blockquote">“You sound like you’re from London” </blockquote>
 
-          <p>His father was a foreigner, a Frenchman, who had trained in the ways of the heart here, in Paris, the City of Love, before expatriating to the States, where he remained a cardiologist but became an American, watching NASCAR on weekends and starting a family, first and foremost with the birth of a boy, a boy who he called Johnny, and who, when he was presented to the world, the world called Science. Johnny. Science. Johnny Science.</p>
+          <p>Something funny we’ve noticed:</p>
 
-          <p>He raised the boy in his image. He drove him to school in a mobile cath lab, until, bored of it, the boy left school altogether and cut his teeth in the clinic, taking blood pressures for pennies and listening to heart murmurs the way other kids listened to Kidz Bop. By the tender age of seven, the boy had showed such potential that the father had got the idea in his head to start to train him seriously. And so, one night, he took the boy by the hand, grabbed some pots and pans, and sat him outside in the midst of a late summer downpour. He tossed a pile of electrocardiograms into the boy’s lap. “Read them,” he said, and began to smash the pots and pans together. The boy looked at his father, looked up at the sky, looked down at the pile of EKGs. And he started reading.</p>
+          <p>You speak with someone who’s learning your language. “I’m sorry for my accent,” they tell you. They seem embarrassed, genuinely apologetic. “But your accent isn’t bad,” you reply. “It’s actually quite nice.” And you mean that, too: English never sounds quite so nice as when it’s spoken by a non-native speaker. (We have discovered, for instance, that French speakers who learned English from Brits speak their second language with a hybrid French-British accent, which is more fun than you can imagine.)</p>
 
-          <p>They did this every time it rained. And in those moments, outside, under the pouring heavens, when the father taught his son everything he knew, he began with the beginning. “Cherchez le ‘p,’” he muttered in his native tongue, almost under his breath, like a prayer. “Cherchez le ‘p.’”</p>
+          <p>And then there’s the reverse. You start to speak another language. You learn the vocabulary, practice your pronunciation until it’s perfect, and finally, when you get the chance, you speak it out loud to a native speaker. You’re nervous at first—of course you are—but as you start to get the words out, you realize you’re not actually doing all that bad. If anything, you sound pretty good. You finish your sentence and stand back, as you’re impressed with yourself. And for a moment, it seems as though everyone else is, too. They’ve all stopped, in fact, in stunned silence. And then, all of a sudden, you realize that it isn’t that they’re impressed. They stare at you, stopping long enough only to stare at one another, before they turn back once again to stare at you.</p>
 
-          <p>The phrase, like the father, came from France. “Cherchez la femme, pardieu!” wrote Dumas. “Cherchez la femme!” And, elsewhere: “Il y a une femme dans toutes les affaires; aussitot qu’on me fait un rapport, je dis ‘cherchez la femme!’” From there, it found its way into film noir, a tradition which finds a female, a femme fatale, at the dark heart of every mystery. And it was the father who, in the spaces in between shifts at the hospital and late night detective stories, found the phrase, reads its rhythm, resurrected it, rehabilitated it for his own purposes, for the dark hearts at the center of his own living, breathing, congestive mysteries. He was Nick Charles in a white coat, Philip Marlowe with a stethoscope. Because he followed leads too, you see: aVR, aVL, and aVF, to be exact, with V1-V6 thrown in to boot, wiretaps to help him find the pulse, pathways etched on paper to his telltale hearts.</p>
+          <p>“Quoi?” One of them asks.</p>
 
-          <p>But no longer was it “cherchez la femme.” No, here, it became “cherchez le ‘p,’” the p-wave, the inciting event, atrial depolarization—a burst of energy that kicks off the story, starting the squeeze that eventually leads to blood. And it was the p-wave, the father found, that held all the secrets. “Cherchez le ‘p,’” he told himself for years, and the secrets to EKGs yielded to him as though they were pulp fiction and he were Humphrey Bogart. He said it to himself. He said it to his students. And he said it to his son, Johnny Science, the Prince of Hearts, outside, in the pouring rain, night after night, as he taught the boy to read EKGs. </p>
+          <p>You repeat yourself, but to no avail. You try a second time, a third. Nothing. And then one of them realizes what you’re trying to say, and, with a great extended “Ohhhhhhh,” they repeat it back to you. It sounds <i>exactly</i> the same to you as what you just said, but empirically, it obviously cannot be. The evidence is in, and you can’t tell the difference. </p>
 
-          <p>And read EKGs the boy did. By the end of the year, he was reading at a medical school level. By the time he was in medical school, he wasn’t even reading the EKGs anymore—they were reading him. But medicine wasn’t everything, and Johnny was a renaissance man. He had a variety of interests, the arts among them, and in the early 2020s, he began to record and transmit his thoughts on emerging musical artists. It was thus that, in the spring of 2021, he re-wrote his father’s phrase. It was still “cherchez,” of course—it was always “cherchez”—but no longer was it the ‘p.’ No, everything he had learned over the year which preceded was that, if you wanted to find good music, you had to “cherchez” something else. You had to “cherchez le Strange,” as in, Bartees. If you find Bartees Strange, you’ll find the music you’re looking for. After “Mustang” and “Goldbrick Champion,” the additions on this week’s WMT prove it.</p>
+          <p>Having this experience multiple times every day has taught us something: you can’t hear your own accent. (You may recognize a variant of this, which is that no one thinks of themselves as speaking with an accent. An accent is only ever something that <i>other</i> people have.) You can tell when you’re <i>mispronouncing</i> things, but you can’t really detect your own accent, which is something else entirely.</p>
+
+          <p>It reminds us of a middle school assignment we once had. We were paired with classmates and asked to record a “news report” on a current event. To our luck (or lack thereof), we were paired with the prettiest girl in the class—a situation which finally forced us into conversation with her. We worked on the report together and, when it was ready, we made the recording. Afterwards we played it back to make sure it sounded okay. As we heard our voice coming through the tape recorder, we cringed. It sounded awful! Somehow throaty and nasally at the same time, as though Louis Armstrong had caught cold and then pinched his nostrils shut in a cannonball dive to boot. The girl shifted uncomfortably in her seat. <i>Quick</i>, we thought to ourselves. <i>You have to do something, anything. You have to save yourself from this situation, and win her heart. Girls like self-deprecation—try that!</i></p>
+
+          <p>“Yikes,” we forced out with an awkward laugh. “Is that how my voice sounds on recordings?”</p>
+
+          <p>“What do you mean ‘on recordings?’” she asked, genuinely confused. “I think that’s just how your voice sounds in general.”</p>
 
           <h2 class="section-heading">And now, onto the show...</h2>
           <br>
@@ -59,11 +65,11 @@
 
           <h2 class="section-heading" style="font-size:125%">From the Vault</h2>
 
-          <p>This week, from the vault, we bring you “Trash” by the New York Dolls. Honestly, we can’t remember if we’ve mentioned this track here before or not, but it’s a positively infectious, cross-dressing romp from 1973 that everyone should know.</p>
+          <p>This week, from the vault, we bring you The Gaslight Anthem’s <i>Get Hurt</i> (2014). The first record lead man Brian Fallon made after his divorce and the last one the band made before their break-up, <i>Get Hurt</i> marked a change of direction for the band that confused a lot of critics at the time. That being said, there’s a lot to like on this record. Honestly, the entire front side of it is a pretty rewarding listen, and latter side tracks like “Ain’t That a Shame” and “Dark Places” will stick with you as well. This marks one of the rare times, moreover, that a deluxe edition actually delivers. “Mama’s Boys” and “Halloween” both are worth your while, as is “Have Mercy” from the iTunes Store deluxe edition. In all, Get Hurt isn’t <i>Sink or Swim</i>—Gaslight’s first and greatest record—but it’s a worthy entry in their catalog and would have received effusive praise coming from any other group.</p>
 
           <h2 class="section-heading" style="font-size:125%">We're Still Listening To</h2>
 
-          <p>Sincere Engineer’s <i>Rhombithian</i> (obviously), Ethel Cain’s “Crush” (duh), and Broke Royals’/Bartees Strange’s “Goldbrick Champion” (but of course)</p>
+          <p>Harmony Woods’ <i>Graceful Rage</i>, Sincere Engineer’s <i>Rhombithian</i>, and Heart Racer’s “Monsters”</p>
 
          </div>
         
@@ -88,8 +94,8 @@
   <script src="js/auto_article_header.js?uid=<?php echo uniqid(); ?>"></script>
   
   <script>
-	displayPosts('2021-04-19');
-  displayArticleHeader('2021-04-19');
+	displayPosts('2021-04-26');
+  displayArticleHeader('2021-04-26');
   </script>
 
 </body>
