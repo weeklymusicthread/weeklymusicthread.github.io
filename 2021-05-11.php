31f7fa7f8623f9e864953bc9c86f76faafacf9e2
Sam <sam.paderewski@gmail.com>
2021-05-12 09:54:30 -0600

    new post

---
+++ 2021-05-11.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head id="pageHead">
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+  
+  <link rel="icon" type="image/x-icon" href="img/favicon.ico" />
+
+  <title>Weekly Music Thread | May 11, 2021</title>
+
+  <?php 
+        include('php/header.php'); 
+        genTwitterCard('2021-05-11');
+  ?>
+
+</head>
+
+<body>
+
+  <?php include('php/navbar.php'); ?>  
+
+  <!-- Page Header -->
+  <header id="articleHeader">
+
+  </header>
+
+  <!-- Post Content -->
+  <article>
+    <div class="container">
+      <div class="row">
+	  	  
+        <div class="col-lg-10 col-md-10 mx-auto" id="postSection">
+
+          <blockquote class="blockquote">'It's then I smell smoke. "You even smoke while you brush your teeth?" She looks at me sideways. "Menthol," she says.' - Janet Woodcock, Meddlesex</blockquote>
+
+          <p>Well, loyal readers, as you may have noticed, we've been away for a bit--but just a bit. You can rest assured that we didn't leave; we only stepped outside. You can call it a smoke break. </p>
+          
+          <p>While we were out, the U.S. Food and Drug Administration (FDA) announced that it intends to ban menthol cigarettes within the next year. Now, we know what your next question is going to be: what in the heck is the U.S. Food and Drug Administration (FDA)? (You could, of course, be forgiven for thinking that here is an organization which administers food and drug; the truth is, kind of, the opposite.) It's too complicated to go into here, but basically, they tell you what sort of things you can and can't light on fire and then breathe in. </p>
+          
+          <p>For instance, if you dried out some tobacco, you could light that on fire and then breathe it in. But if you dried out some tobacco and then soaked it in mint, you could NOT light that on fire and then breathe it in. </p>
+          
+          <p>Now, as anyone who has ever heard music will know, things that you can light on fire and then breathe it in don't only have their place between our lips and fingers--they've also got their place in our songs and lyrics. Even their paraphernalia have a presence. Doesn't the U.S. Food and Drug Administration (FDA) remember when we used to go to concerts and wave our lighters in the air? Or do they just not care?</p>
+          
+          <p>But seriously--let's forget cigarettes in general, and focus on those menthols for a moment. A simple Google search of "menthols song lyrics" returns hundreds of songs built on cornerstones of packets of Newports. And none more meaningful to us than one that takes us back to London in 1978, with the sophomore effort of a little band you may have heard of called The Clash. Give 'Em Enough Rope. Side two. Track three. "Stay Free." "Got me in bad fights/play me pool all night," sings Joe Strummer. "Smokin' menthols." ("Stay Free." What a prescient, libertarian protest anthem.)</p>
+          
+          <p>The U.S. Food and Drug Administration (FDA) would deprive us of menthols. This, we know. Would they also deprive us of "Stay Free?" Of The Clash? To that, we're sure, they'd say, "wait, who?"</p>
+          
+          <p>No, not The Who, The Clash. No doubt the folks at the U.S. Food and Drug Administration have never heard of either. And yet, they're ready to write out of existence one of the best tracks from the latter. </p>
+          
+          <p>But, on the other hand, when did a government ban ever make anything less cool? Prohibition gave us Gatsby and gangsters, and it's not as though hip hop lyrics have taken a hit as a result of the prohibition of marijuana. (Thalidomide, admittedly, has lost a little of its cache and social currency.)</p>
+          
+          <p>So maybe, then, we've got it backwards. Maybe the U.S. Food and Drug Administration (FDA) isn't out to stub out menthols, but to light a subcultural fire under them. By banning them, they reinvigorate them. They make them cool. Give them a breath of fresh, minty air and, in so doing, Make Menthols Great Again.</p>
+
+          <h2 class="section-heading">And now, onto the show...</h2>
+          <br>
+        </div>
+
+        <div class="col-lg-10 col-md-10 mx-auto"> <!-- From The Vault/We're Stil Listening/Up The Punx-->
+         
+          <hr>
+
+          <h2 class="section-heading" style="font-size:125%">From the Vault</h2>
+
+          <p>This week, from the vault, we bring you another track off of The Clash's <i>Give 'Em Enough Rope</i>: "Safe European Home." If you haven't heard this one before, it is an utter banger--the kind of track you can't help but play over, and over, and over. So, you know. You're welcome.</p>
+
+          <h2 class="section-heading" style="font-size:125%">We're Still Listening To</h2>
+
+          <p>Porter Robinson's <i>Nurture</i>, Sincere Engineer's <i>Rhombithian</i>, Harmony Woods' <i>GRACEFUL RAGE</i>, Hit Like a Girl's "Monsters", and RMR's "RASCAL"</p>
+
+         </div>
+        
+		
+      </div>
+    </div>
+  </article>
+
+  <hr>
+
+  <?php include('php/footer.php'); ?>  
+
+  <!-- Bootstrap core JavaScript -->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  <!-- Custom scripts for this template -->
+  <script src="js/clean-blog.js?uid=<?php echo uniqid(); ?>"></script>
+  
+  <script src="js/parse_music_entries.js?uid=<?php echo uniqid(); ?>"></script>
+
+  <script src="js/auto_article_header.js?uid=<?php echo uniqid(); ?>"></script>
+  
+  <script>
+	displayPosts('2021-05-11');
+  displayArticleHeader('2021-05-11');
+  </script>
+
+</body>
+
+</html>
