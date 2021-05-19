9ebae4c54ec6faf2c3432d3bfbe359bfb9146e1f
Sam <sam.paderewski@gmail.com>
2021-05-19 16:40:59 -0600

    new post

---
+++ 2021-05-17.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head id="pageHead">
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+  
+  <link rel="icon" type="image/x-icon" href="img/favicon.ico" />
+
+  <title>Weekly Music Thread | May 17, 2021</title>
+
+  <?php 
+        include('php/header.php'); 
+        genTwitterCard('2021-05-17');
+  ?>
+
+</head>
+
+<body>
+
+  <?php include('php/navbar.php'); ?>  
+
+  <!-- Page Header -->
+  <header id="articleHeader">
+
+  </header>
+
+  <!-- Post Content -->
+  <article>
+    <div class="container">
+      <div class="row">
+	  	  
+        <div class="col-lg-10 col-md-10 mx-auto" id="postSection">
+
+          <blockquote class="blockquote">“Don’t really care for classical music; lot of unnecessary notes” – Charlie Kelly, <a href="https://www.youtube.com/watch?app=desktop&v=Q3vN4N8LbLU">almost</a></blockquote>
+
+          <p>“Where now? When now? Who now? Unquestioning. I, say I. Unbelieving. Questions, hypotheses, call them that. Keep going, going on, call that going, call that on.” </p>
+          
+          <p>So opens <i>The Unnamable</i>, the third installment in Samuel Beckett’s trilogy. It’s one of the great opening lines of all time—perhaps the great opening line, given that it achieves a level of meta-awareness that would never occur to most others even to attempt. Put otherwise, opening lines are always about <i>something</i>, but rarely are they about opening lines themselves. Set a scene, introduce some characters. Imply a personality and you create it within the reader’s mind—unquestioning. All you have to do is say “I,” and keep going from there.</p>
+          
+          <p>About 20 years after <i>The Unnamable</i>, another author—Samuel R. Delany—gave us something similar in the opening lines of his circular, post-modern science fiction epic, <i>Dhalgren</i> (which we recommend reading while on honeymoon in Japan). “to wound the autumnal city,” the novel begins. “<b>So howled out for the world to give him a name</b>. The in-dark answered with wind. <b>All you know I know.</b>”</p>
+          
+          <p>The modernists and post-modernists played extensively with this idea—that our language constructs our reality. This, of course, did not come solely or even primarily from fiction writers like Beckett and Delany, but also from post-modern philosophers. (Even Noam Chomsky, no fan of the post-modernists, tells us that “the structure of language determines not only thought, but reality itself.”) This idea is so powerful that to some it has become almost commonplace, taken for granted. It crops up everywhere you look. It’s in Orwell’s <i>Nineteen Eighty-Four</i> (Big Brother controls its subjects by winnowing down their language and thus depriving them of the words they need to construct certain thoughts) and even the Harry Potter books, in which characters avoid using the villain’s name as though doing so will speak him into being. To make something real, we’ve been taught, you need to speak it into being.</p>
+          
+          <p>Indeed, that’s where this very thread came from. A New Year’s Resolution a year and a half ago to try and discover, more regularly, great new music to share. In order to hold ourselves to it, we started the thread (which actually did start as a thread, a weekly e-mail between a few friends), because it was easier to force ourselves to find new music when we knew we had to write about it once a week. And it worked: not only have we found plenty of wonderful new music (hello, Ethel Cain), but we’ve found a way to share it with other people, too.</p>
+          
+          <p>Well, now it’s time to speak something else into being. Not something else entirely, but something related, something WMT-adjacent. And that’s a new sub-section, which we’ll be calling the Classical Fascicle. You see, we’ve wanted to become more fluent in classical music for years--decades, even, at this point—but we’ve never gotten it to stick. There have been a few pieces that have held our attention, famous ones like the 1812 Overture and Beethoven’s Ninth, but for the most part, this entire <i>world</i> of art has eluded us. Well, no longer. We’re committing here and now to further expanding the Weekly Music Thread by including one piece of classical music per week.</p>
+          
+          <p>Now, it’s important to keep in mind that we have no idea what we’re doing and little clue what we’re talking about. This is, of course, <i>generally</i> true, but it goes ten-fold for classical music. Can you really speak something into being if you’re functionally mute? We suppose that we’re going to find out. We anticipate that this will come off as something of a cross between <a href="https://www.youtube.com/watch?app=desktop&v=nQVnRKIpzAU">Zoidberg’s art collection</a> and <a href="https://www.youtube.com/watch?app=desktop&v=r73C0N8FDek">Leonard’s frozen pizza reviews</a>. But if it’s anything like the WMT itself, we’ll look back in 18 months and be both glad we did it and surprised at how far it’s come. </p>
+
+          <h2 class="section-heading">And now, onto the show...</h2>
+          <br>
+        </div>
+
+        <div class="col-lg-10 col-md-10 mx-auto"> <!-- From The Vault/We're Stil Listening/Up The Punx-->
+         
+          <hr>
+
+          <h2 class="section-heading" style="font-size:125%">From the Vault</h2>
+
+          <p>This week, from the vault, we bring “Jenny Was a Friend of Mine” by The Killers. The opening track off of their 2004 debut Hot Fuss, this track is sometimes overshadowed by “Somebody Told Me” and “All These Things That I’ve Done.” From first listen, however, this one—inspired by a real-life murder confession and told from the perspective of a jealous boyfriend being questioned about the circumstances surrounding his girlfriend’s death—instantly grabbed, and then always held, our attention. It’s an excellent opener to an excellent record, even if it’s more raw than everything that comes after it put together. The screaming synths, the churning drums, and that thick, fatty, chewy, bloody, rare-almost-raw bass line. Perhaps the most interesting thing about “Jenny Was a Friend of Mine” is that it poses a question that it never answers. Our narrator tells us that “she couldn’t scream while I held her close/I swore I’d never let her go.” Did he do it? Didn’t he? (In live performances, Flowers places his finger on the scales when instead he sings “she couldn’t scream while I held her throat” or even “she kicked and screamed while I held her throat.”) Listen to this one on repeat while you try to decide.</p>
+
+          <h2 class="section-heading" style="font-size:125%">The Classical Fascicle</h2>
+
+          <p>Well, folks, here it is: the first installment of the classical fascicle. The entire idea, as described above, was actually born in response to this week’s piece: Sergei Rachmaninoff’s Isle of the Dead. (We’ve been listening to the 1984 Vladimir Ashkenazy/Concertgebouworkest recording.) First of all, there is a backstory to this piece that existing fans of classical music probably already know well. Rachmaninoff was inspired to create it upon seeing a black-and-white print of the famous Bocklin painting by the same name. Later, however, upon seeing the image in color, Rachmaninoff remarked that it disappointed him, and that he probably would not have been inspired to write the piece had he seen the color version first. Lucky for us, we suppose, he didn’t.</p>
+
+          <img src="img/bocklin1.jpg" alt="Bocklin 1">
+
+          <img src="img/bocklin2.jpg" alt="Bocklin 2">
+
+          <p>(P.S. Next time someone scoffs at a director’s decision to film in black-and-white, show them these two images and see if they don’t think it makes a tonal difference. Clearly, it did for Rachmaninoff. Actually, you can also show them Season 1, Episode 2 of the utterly brilliant <i>Neo Yokio</i>, which briefly features this very painting with a midnight blue sky during its climactic fight scene.) As a brief aside, our first introduction to Rachmaninoff must have been 16 or 17 years ago when, one summer day, a diehard metal head and a buttoned-up financier with whom we were lunching bonded over his Prelude in C Sharp Minor. We ran into him (Rachmaninoff) again this week, with Isle of the Dead. From its opening moments, it is dark, cinematic, and foreboding. (It’s also a <i>clear</i> influence on the quieter and more somber moments of Clint Mansell’s “Death is the Road to Awe.” In fact, for those who, like us, have had a hard time breaking into orchestral music, the Mansell track is not a bad place to start. It’s best listened to loud, on very nice headphones, with eyes closed, recumbent, in the dark.) It’s said that the opening movement of Isle of the Dead is meant to evoke the feeling of approaching the island, the sound and rhythm of the oars on the water, or even the waves themselves. The entire piece alternates between edgy and severe, and it really held our attention. We felt like it was accessible, and maybe you’ll feel the same. To be honest, we’re not really sure what else to write here. After all, we barely know how to listen to classical music; how are we supposed to write about it? We’re beginning to realize that this may not have been the best idea…</p>
+
+          <h2 class="section-heading" style="font-size:125%">We're Still Listening To</h2>
+
+          <p>Porter Robinson’s <i>Nurture</i>, Harmony Woods’ <i>GRACEFUL RAGE</i>, Sincere Engineer’s <i>Rhombithian</i>, and Hit Like a Girl’s “Monsters”</p>
+
+         </div>
+        
+		
+      </div>
+    </div>
+  </article>
+
+  <hr>
+
+  <?php include('php/footer.php'); ?>  
+
+  <!-- Bootstrap core JavaScript -->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  <!-- Custom scripts for this template -->
+  <script src="js/clean-blog.js?uid=<?php echo uniqid(); ?>"></script>
+  
+  <script src="js/parse_music_entries.js?uid=<?php echo uniqid(); ?>"></script>
+
+  <script src="js/auto_article_header.js?uid=<?php echo uniqid(); ?>"></script>
+  
+  <script>
+	displayPosts('2021-05-11');
+  displayArticleHeader('2021-05-11');
+  </script>
+
+</body>
+
+</html>
