1865390fc5ed921f399a96a274479fe7da280b32
Sam <sam.paderewski@gmail.com>
2021-06-18 18:40:42 +0200

    new post

---
+++ 2021-06-18.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head id="pageHead">
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+  
+  <link rel="icon" type="image/x-icon" href="img/favicon.ico" />
+
+  <title>Weekly Music Thread | June 18, 2021</title>
+
+  <?php 
+        include('php/header.php'); 
+        genTwitterCard('2021-06-18');
+  ?>
+
+</head>
+
+<body>
+
+  <?php include('php/navbar.php'); ?>  
+
+  <!-- Page Header -->
+  <header id="articleHeader">
+
+  </header>
+
+  <!-- Post Content -->
+  <article>
+    <div class="container">
+      <div class="row">
+	  	  
+        <div class="col-lg-10 col-md-10 mx-auto" id="postSection">
+
+          <p>Doing something for the first time is always… weird. Take playing basketball, or even dribbling a basketball, for that matter. You try it and come to a quick realization of, “wow, I suck at this.” Oftentimes these experiences are uncomfortable, and it is only after hours upon hours of time spent with such an endeavor that you can say, “well, at least I suck less now.”</p>
+
+          <p>This idea is taken to great heights in <i>The 40-Year-Old Virgin</i>, when Jay advises Andy to basically court “hood rats”, so when he does meet someone he actually likes, he can at least be mediocre at coitus.</p>
+
+          <p>This raises a clear and obvious question: where do we draw the line between being a novice and intermediate at something? Is there a line? Or perhaps, is it more of a spectrum-like gradient, where your increasing experience shades in what was initially a blank slate?</p>
+
+          <p>After finishing a challenging book, we at the WMT always feel as though we were illiterate before reading it, and that anything we read prior to that book was a total waste. Surely, we always say to ourselves, there were references we missed, words whose implications we didn’t quite understand. This internal war of attrition isn’t something that ends, or something that one should want to end. How would you get any better at understanding existentialist philosophy without reading <i>Being and Nothingness</i>? (For the record, we had the above feeling of illiteracy before, during, and after reading that behemoth.) But <i>that</i> is exactly what we’re saying.</p>
+
+          <p>Getting good at anything takes time. Whether it’s dribbling a basketball, reading Sartre, or writing a music blog post, the first time you’ll probably suck, and the next time should be better.</p>
+
+
+          <h2 class="section-heading">And now, onto the show...</h2>
+          <br>
+        </div>
+
+        <div class="col-lg-10 col-md-10 mx-auto"> <!-- From The Vault/We're Stil Listening/Up The Punx-->
+         
+          <hr>
+
+         </div>
+        
+		
+      </div>
+    </div>
+  </article>
+
+  <hr>
+
+  <?php include('php/footer.php'); ?>  
+
+  <!-- Bootstrap core JavaScript -->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  <!-- Custom scripts for this template -->
+  <script src="js/clean-blog.js?uid=<?php echo uniqid(); ?>"></script>
+  
+  <script src="js/parse_music_entries.js?uid=<?php echo uniqid(); ?>"></script>
+
+  <script src="js/auto_article_header.js?uid=<?php echo uniqid(); ?>"></script>
+  
+  <script>
+	displayPosts('2021-06-18');
+  displayArticleHeader('2021-06-18');
+  </script>
+
+</body>
+
+</html>
