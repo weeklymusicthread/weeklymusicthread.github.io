3732e963e9d24f43be01787f40739a01839b94b9
Sam <sam.paderewski@gmail.com>
2021-06-29 19:18:42 +0200

    new post

---
+++ 2021-06-29.php
@@ -0,0 +1,119 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head id="pageHead">
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+  
+  <link rel="icon" type="image/x-icon" href="img/favicon.ico" />
+
+  <title>Weekly Music Thread | June 29, 2021</title>
+
+  <?php 
+        include('php/header.php'); 
+        genTwitterCard('2021-06-29');
+  ?>
+
+</head>
+
+<body>
+
+  <?php include('php/navbar.php'); ?>  
+
+  <!-- Page Header -->
+  <header id="articleHeader">
+
+  </header>
+
+  <!-- Post Content -->
+  <article>
+    <div class="container">
+      <div class="row">
+	  	  
+        <div class="col-lg-10 col-md-10 mx-auto" id="postSection">
+
+          <blockquote class="blockquote">“[Conservatism] is standing athwart history, yelling ‘WAP!’” – Cardi F. Buckley</blockquote>
+
+          <p>First of all, apologies for our absence as of late! All of half of the WMT has been gallivanting around Europe, enjoying daily and nightly ice-cold Toms Cruise (Davids Kronenbourg and Nikoles Hannah-Jones, to be exact.) At any rate, the prodigal sons have returned, and they’ve come back well in hand with what you’ve always wanted yet never knew you needed: an analysis of Shaggy and Rayvon’s “Angel.”</p>
+          
+          <p>When we recently re-discovered “Angel,” we were amused to realize that the song samples heavily from two tracks we’ve always loved: Steve Miller Band’s “The Joker,” and Juice Newton’s “Angel of the Morning.” We can be forgiven for missing this the first time around. (We were, after all, in, like, middle school at the time. We remember that because we remember our mother confiscating our copy of <i>Hot Shot</i> when she finally made out the word “incarceration” after several hundred or so listens. One way to get around censorship, it turns out, is to deliver your lyrics completely unintelligibly.) We didn’t really discover the Newton track until it showed up in the <a href=”https://www.youtube.com/watch?v=CZe3X-EaMis”>first season of HBO’s <i>True Detective</i></a>, and we didn’t really discover Steve Miller until he showed up in the Trump administration. </p>
+          
+          <p>The blending of “The Joker” and “Angel of the Morning” works. It’s sort of like mixing together ketchup, mustard, and mayonnaise: once you finally think to blend them together, you wonder why you spent so many years enjoying these things in isolation. And the proof is in the salmon-colored pudding: Shaggy’s “Angel” reached the number one spot on the US <i>Billboard</i> Hot 100. (To do so, it barely edged out Crazy Town’s cunnilingus anthem, “Butterfly.”) All of those things, it turned out, just went well together.</p>
+          
+          <p>Now, that’s not to say that there aren’t differences between them. There are, and the most glaring ones come in the lyrics. The lines from “The Joker,” for instance, are full of silliness and carefree whimsy. (“I really love your peaches, wanna shake your tree.”) “Angel of the Morning,” by contrast, is full of conflict, uncertainty, mixed messaging, and a thinly veiled sense of self-doubt. (“Maybe the sun’s light will be dim/and it won’t matter anyhow/if morning’s echo says we’ve sinned/well, it was what I wanted now/And if we’re victims of the night/I won’t be blinded by the night.”) Interestingly, Shaggy’s “Angel” somehow models the most mature take on sexual/romantic relationships out of the three, which is all the more surprising when you consider that it’s delivered with the hot-potato voice of an epiglottitic and the syntactical stylings of Yoda. See for yourself:</p>
+          
+          <p style = "padding-left: 2em"><u>Angel of the Morning (chorus)</u></p>
+
+          <p style = "padding-left: 2em"><i>Just call me angel of the morning, angel<br>Just touch my cheek before you leave me, baby<br>Just call me angel of the morning, angel<br>Then slowly turn away from me<br>I won’t beg you to stay</i></p>
+          
+          <p style = "padding-left: 2em"><u>Angel (chorus)</u></p>
+
+          <p style = "padding-left: 2em"><i>Girl you’re my angel, you’re my darling angel<br>Closer to my peeps than you are to me, baby <-- Yoda syntax<br>Shorty you’re my angel, you’re my darling angel<br>Girl you’re my friend when I’m in need, lady</i></p>
+                    
+          <p>Perhaps it’s because we were so young at the time—and, therefore, not ready for songs about loving, monogamous relationships—but parts of “Angel” were mysteries to us for years. Take, for example, the opening verse: “Life is one big party when you’re still young/but who’s gonna have your back when it’s all done/it’s all good when you’re little you have pure fun/can’t be a <b>fool</b> son what about the long run?” As our mother drove us to and from extracurricular activities, oblivious to the song’s impending reference to incarceration, we were busy bobbing our head in the backseat and hearing that last line as “can’t be <b>Apu</b> son what about the long run?”</p>
+          
+          <p>This made a certain amount of sense to us at the time. As a fan of <i>The Simpsons</i>, we interpreted the line to mean that ample planning and foresight could secure us safer and better compensated careers than convenience store ownership. Like we said, we were young, and we had not yet been exposed to the American lionization of small business owners. We hadn’t yet seen a presidential debate. What’s more, our eyes were completely closed to the fact that, as a self-employed owner of a small business, Apu likely had <i>many</i> opportunities—in the form of tax-advantaged retirement accounts—to plan for the long run: traditional IRAs, Roth IRAs, SEP IRAs, SIMPLE IRAs, and solo 401(k)s. Shit, he could even have gotten some really nice tax-protected growth out of his HSA, and wouldn’t even have had to worry about a phase-out if that convenience store career really did take off. And once he maxed out all of those contributions, he could always go with a traditional, taxable brokerage account, or even explore real estate. At a certain point, <a href=”https://www.youtube.com/watch?app=desktop&v=8mQYcyxnFyE”>BJ Novak even could have explained to him that he wasn’t in the Kwik-E-Mart business…he was in the <i>real estate business</i></a>. </p>
+          
+          <p>Anyway, are we Nirvana rn? Because never mind. What’s really interesting about the way that “The Joker,” “Angel of the Morning,” and “Angel” interact is what we touched on earlier: their subject matter, and their overall messages vis-à-vis romantic relationships. Let’s consider them pseudo-chronologically.</p>
+          
+          <p>We’ll start with “The Joker.” (Like we said, <i>pseudo</i>-chronologically. More on that below.) “The Joker” is full of lines that betray a certain libertinism: “some call me the gangster of love,” “people talk about me baby/say I’m doin’ you wrong, doin’ you wrong,” “I’ll sure show you a good time,” and, perhaps most obvious of all of them, “I get my lovin’ on the run.” Honestly, as early as the first listen, it seems fairly obvious that “The Joker,” which was written in 1973 smack-dab in the middle of the two decades-long sexual liberation movement, celebrates a certain view of sexual relationship as easygoing and no-strings-attached.</p>
+          
+          <p>Now, let’s move to the next track: Juice Newton’s “Angel of the Morning.” Although it’s technically true that the song was originally written by Chip Taylor in 1967, multiple artists passed on it for fear that its subject material (an illicit affair) would do harm to their public reputations. By the time Newton took it up, it was already 1981—nearly a decade after Steve Miller was playing his music in the sun—and the sexual liberation movement was beginning to lose its vigor. Newton’s take is therefore an interesting one. The song feels insistent—“there’ll be no strings to bind your hands,” “just call me angel,” “I won’t beg you to stay,” “I’m old enough to face the dawn”—and yet somehow reluctant at the same time. (Are we Queen Gertrude rn? Because this lady doth protest too much, wethinks.) Rather than truly sexually liberated, in other words, the narrator of Newton’s “Angel of the Morning” seems to us merely to be playing a part. (Is it even <i>possible</i> to be sexually “liberated”? That is…probably a topic for something other than an amateur and nonsensical quasi-regular music blog.) She’s saying all of the things she thinks she is supposed to say to her paramour—we’re honestly surprised the phrase “friends with benefits” doesn’t show up—but she doesn’t seem deep down really to believe them herself. How do we know that? Because she tells us as much in the interlude: “through the tears/of the days/of the years, baby.” It’s subtle, but we feel fairly confident that “Angel of the Morning” starts to anticipate the cynical dissatisfaction that some women would feel in the wake of the sexual revolution.  </p>
+          
+          <p>Enter Shaggy, Rayvon, “Angel.” Whereas “The Joker” gave us a liberated (if not licentious) male lover, and “Angel of the Morning” provided us with his conflicted and reluctant progressive female counterpart, “Angel” comes along and offers us a reformed male narrator who commits joyfully to a loving and monogamous relationship with his longtime girlfriend. It’s the conservative pipe dream: a popular media return to traditional values and the chastened nuclear family. (The fact that “Angel” followed Hot Shot’s other hit single, “It Wasn’t Me,” speaks even further to this redemption turn.) To be honest, pretty much every line in the song speaks to the idea of building a long-term, trusting relationship with a single partner: “you’re my friend when I’m in need,” “who’s gonna have your back when it’s all done,” “what about the long run,” “she was there through my incarceration,” “you’re a queen and that’s how you should be treated,” and, finally, “the feeling that I have for you is so strong/been together so long/this could never be wrong.” And while “Angel” is an ode to the narrator’s lover, love exists here only as an emotion, and never as a physical act. Sexuality, in other words, is almost entirely absent. The song is squeaky clean (our mother’s least favorite line about incarceration, we suppose, notwithstanding). You could play it in church. You could even play it at the RNC. And so, incredibly, as we move from “The Joker” (1973) to “Angel of the Morning” (1981) to “Angel” (2001), this spontaneous and incidental triptych becomes, in its socio-sexual messaging, increasingly conservative and traditional.</p>
+          
+          <p>Now, is that what Shaggy was after all along? Did Shaggy merely sample “The Joker” and “Angel of the Morning” for their sounds alone, or did he intentionally use these two tracks to move popular music toward a more traditional appreciation of monogamous romance? And did he lead things off with “It Wasn’t Me” specifically in order to set up this surprising conservative turn? For an immigrant ex-Marine who’s on record ambiguously saying that the values that drew him to the United States are under attack, it seems at least a little bit more possible than we imagined back in middle school when we were locked in a death-grip tug-of-war with our then-censorious mother (“Tipper Gore!,” we shouted at her) over our copy of <i>Hot Shot</i>. We may never know the truth, but we do know this: there’s a universe in which Shaggy did this all by design, and a universe in which he didn’t. And we know which one we’d prefer to inhabit. </p>
+          
+          <h2 class="section-heading">And now, onto the show...</h2>
+          <br>
+        </div>
+
+        <div class="col-lg-10 col-md-10 mx-auto"> <!-- From The Vault/We're Stil Listening/Up The Punx-->
+         
+          <hr>
+
+          <h2 class="section-heading" style="font-size:125%">From the Vault</h2>
+
+          <p>This week, from the vault, we bring you an offering that’s well in line with both summer time and the faux-reggae stylings of this edition’s introduction: Peter Tosh’s “Wanted Dread & Alive.” You may know this one from <i>Pineapple Express</i> (2008). It’s a fun and carefree track that pairs well with Toms Cruise, Davids Kronenbourg, and Nikoles Hannah-Jones. We know from experience.</p>
+
+          <h2 class="section-heading" style="font-size:125%">The Classical Fascicle</h2>
+
+          <p>A brief entry this week, lifted from Lars von Trier’s <i>Antichrist</i> (2009): the Tuva Semmingsen and Barokksolistene performance of George Frideric Handel’s <i>Lascia Ch’io Pianga</i>. At 5:18, this is a short soprano aria that’s approachable and easily consumed (<i>Antichrist<i>, on the other hand, not so much). The piece is beautiful, which belies the wrenching pain expressed in the Italian lyrics: “let me weep over my cruel fate/and let me sigh for liberty/may sorrow shatter these chains/for my torments just out of pity.” Best listened to in a warm, solitary, candlelit bath. </p>
+
+          <h2 class="section-heading" style="font-size:125%">We're Still Listening To</h2>
+
+          <p>Pearly Drops’ “Final Form,” Jetty Bones’ “No Lover” and “That’s All,” and Videoclub’s “Enfance 80” </p>
+
+         </div>
+        
+		
+      </div>
+    </div>
+  </article>
+
+  <hr>
+
+  <?php include('php/footer.php'); ?>  
+
+  <!-- Bootstrap core JavaScript -->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  <!-- Custom scripts for this template -->
+  <script src="js/clean-blog.js?uid=<?php echo uniqid(); ?>"></script>
+  
+  <script src="js/parse_music_entries.js?uid=<?php echo uniqid(); ?>"></script>
+
+  <script src="js/auto_article_header.js?uid=<?php echo uniqid(); ?>"></script>
+  
+  <script>
+	displayPosts('2021-06-29');
+  displayArticleHeader('2021-06-29');
+  </script>
+
+</body>
+
+</html>
