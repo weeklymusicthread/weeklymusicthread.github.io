412d5200bd2f99e64766d4ba725a142c109c13c8
Sam <sam.paderewski@gmail.com>
2021-08-28 16:41:22 +0100

    new post

---
+++ 2021-08-28.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head id="pageHead">
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+  
+  <link rel="icon" type="image/x-icon" href="img/favicon.ico" />
+
+  <title>Weekly Music Thread | August 28, 2021</title>
+
+  <?php 
+        include('php/header.php'); 
+        genTwitterCard('2021-08-28');
+  ?>
+
+</head>
+
+<body>
+
+  <?php include('php/navbar.php'); ?>  
+
+  <!-- Page Header -->
+  <header id="articleHeader">
+
+  </header>
+
+  <!-- Post Content -->
+  <article>
+    <div class="container">
+      <div class="row">
+	  	  
+        <div class="col-lg-10 col-md-10 mx-auto" id="postSection">
+
+          <blockquote class="blockquote">“And we don’t go nowhere/without the radio on” – Everyone, apparently</blockquote>
+
+          <p>First of all, we ask that you please excuse our second extended absence of the summer. It’s France, after all—we’ve taken time off.</p>
+
+          <p>Speaking of <i>la France</i>, we think back now, amid <i>les passes sanitaires et TousAntiCovid</i>, to the second half of June—specifically, to the summer solstice. In Paris, this day marks the “Fete de la Musique.” First established in 1982 by then-French Ministry of Culture Director of Music and Dance Maurice Fleuret, the Fete de la Musique—a day on which professional and amateur musicians are encouraged to take to the streets and perform spontaneous shows for the love of music—has since spread to over a hundred different countries. (Although battle lines on the subject once were drawn and then, somehow, crossed, there’s no word yet on whether shutting down international travel sooner would have helped to slow down <i>that</i> particular viral spread.) In English, it’s known as “Music Day,” “Make Music Day,” and/or “World Music Day,” all of which sacrifice the pun in the party’s native language. (In French, “Fete de la Musique”—the Festival of Music—sounds almost identical to “faites de la musique”—an exhortation to “make music.”) The creator once described his vision of the FdlM as “the music everywhere and the concert nowhere.” Although this probably would have been a little punchier back in the Banu Musa heyday of 9th century Baghdad, the idea seems to have been that it’s sort of like Uber, but for street music. Or, maybe a little more accurately, like Spotify, but after the onset of the coronavirus (and after the Advent of digital Fortnite concerts.)</p>
+
+          <p>Of course, we, like you, had no idea about any of this. We’ve spent the last few years actively severing ties with the current events and politics of our native country; the goings-on in France might as well have been Greek to us. The most we can tell you on the <i>political</i> front is that there have been <i>many</i> strikes (it’s France, after all), <i>many</i> more protests (still France), and a whole bunch of people fired up over someone who we can only assume is the wife of actor Sean Penn—some lady named Marina.</p>
+
+          <p>So imagine our surprise when we left work one day, made our way into the streets, and encountered band after band after band packing full the <i>terrasses</i> and flowing over onto sidewalks. Here indie, here ska, here reggae, here garage, and all of them well within the six forbidden feet of one another. Hard to say just what such a thing would do for viral mixing, but it certainly produces what can only be described as a super-spreader miasma of sound.</p>
+
+          <p>Here’s the thing about Paris, though. Indeed, it’s the thing about any major city. If you ride public transit, then what you quickly realize is that <i>Fete de la Musique? C’est toujours</i>. </p>
+
+          <p>Every day, in other words, is the <i>Fete de la Musique</i>.  </p>
+
+          <p>Don’t believe us? It’s easy: just descend into the subway yourself, and all around you people are playing songs and videos aloud from their phones. (A mere mention is enough—you hear it even now, don’t you?) Thin, metallic trebles that sound like tin can telephones. A flatulent bass line that sputters forth like the clarion call of the Malebranche, only updated for the Internet age. All of it comes together in an eco-system—a swamp, maybe—of sounds. Is that what this is? Spontaneous (dis)order? Or perhaps it’s a call and response form of clandestine communication. Is it intended as a public service? And if so, then is it experienced as such? Or are we reading too much into this, and everyone is just always forgetting their headphones on the exact same days that we ride the Metro?</p>
+
+          <p>A side note: the habit does seem almost…<i>anti</i>-correlated with taste. No one’s ever watching Jarmusch, for example. No one’s ever listening to Bach. Or is it just that such tastes are in short supply? After all, we recently traveled internationally (gasp!), and the woman across the aisle from us on the plane watched Jason Statham in <i>The Meg</i> not once, not twice, but <i>three</i> times in a row. And no, she wasn’t just sleeping. (Why does nobody believe this story? Anyway, for the sake of closure, we feel it important to mention that she followed up her Statham triptych with a viewing of <i>Godzilla vs. Kong.</i>) Perhaps, when you consume the <i>gratis</i> media of others around you, you shouldn’t be expecting a luxury experience. Those, we are starting to think, you might have to pay for.</p>
+
+          <p>(And speaking of the plane, it really overturns the headphones hypothesis. They leave a pair of free ones sitting out on every single seat, so whatever this is, it’s not a problem of resources.)</p>
+
+          <p>And this, finally, brings us back to Fleuret, and the idea that he had for the Fete de la Musique in the first place. Although we suppose that a celebration of amateur musicians is all well and good, we also have to point out that, if these people were talented enough that, on balance, their music was worth listening to, well, they probably wouldn’t still be <i>amateurs</i>. </p>
+
+          <h2 class="section-heading">And now, onto the show...</h2>
+          <br>
+        </div>
+
+        <div class="col-lg-10 col-md-10 mx-auto"> <!-- From The Vault/We're Stil Listening/Up The Punx-->
+         
+          <hr>
+
+          <h2 class="section-heading" style="font-size:125%">From the Vault</h2>
+
+          <p>This week, from the vault, we bring you Twin Shadow’s “Brace (feat. Rainsford).” The lead track off of 2018’s <i>Caer</i>, “Brace” is a modern, synth R&B take on Tom Petty’s “Free Fallin’” that <i>really, really</i> works, not least of all on repeat in front of a flow cytometer for late night and/or early morning hours and hours on end. You’ll be all like “yeah, I WAS in the back room, dreaming out loud before I found you!”</p>
+
+          <h2 class="section-heading" style="font-size:125%">The Classical Fascicle</h2>
+
+          <p>This week, as part of the Classical Fantastical, we bring you the overture to Richard Wagner’s <i>Rienzi</i>. One of Wagner’s early operas, <i>Rienzi</i> tells the story of “a late medieval Italian populist figure who succeeds in outwitting and then defeating the nobles and their followers and in raising the power of the people. Magnanimous at first, he is forced by events to crush the nobles’ rebellion against the peoples’ power, but popular opinion changes and even the Church, which had urged him to assert himself, turns against him. In the end the populace burns the Capitol.” It is, in other words, an entire fiction which bears no resemblance to the current day state of affairs—a nice break from the rough-and-tumble world of contemporary politics. The overture is a brief, accessible dream with a few really beautiful moments. Check it out, and ready yourself for more Wagner here in the weeks to come.</p>
+
+          <h2 class="section-heading" style="font-size:125%">We're Still Listening To</h2>
+
+          <p>Phoebe Bridgers’ “Motion Sickness,” Pale Waves’ “Television Romance,” Charli XCX’s “Detonate,” Sir Babygirl’s “Pink Lite,” Glitch Gum’s “Kyoto,” and Taylor Swift’s “willow (‘90s trend remix)”</p>
+
+         </div>
+        
+		
+      </div>
+    </div>
+  </article>
+
+  <hr>
+
+  <?php include('php/footer.php'); ?>  
+
+  <!-- Bootstrap core JavaScript -->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  <!-- Custom scripts for this template -->
+  <script src="js/clean-blog.js?uid=<?php echo uniqid(); ?>"></script>
+  
+  <script src="js/parse_music_entries.js?uid=<?php echo uniqid(); ?>"></script>
+
+  <script src="js/auto_article_header.js?uid=<?php echo uniqid(); ?>"></script>
+  
+  <script>
+	displayPosts('2021-08-28');
+  displayArticleHeader('2021-08-28');
+  </script>
+
+</body>
+
+</html>
